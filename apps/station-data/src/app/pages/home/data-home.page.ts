ed4e6517484f771871842be3dbb38bf40506d153
Chris\Laptop <chris.m.clarke@live.co.uk>
2019-07-02 21:39:39 -0700

    add nx-admin template

--- apps/station-data/src/app/pages/home/data-home.page.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'data-home',
-  templateUrl: './data-home.page.html',
-  styleUrls: ['./data-home.page.scss']
-})
-export class DataHomePage {}
