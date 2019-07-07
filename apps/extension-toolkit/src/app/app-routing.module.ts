9cca6418f713d67e56dacbc4b35a7ecf45ad35e0
Chris\Laptop <chris.m.clarke@live.co.uk>
2019-07-07 16:19:31 -0700

    update notes

--- apps/extension-toolkit/src/app/app-routing.module.ts
+++ apps/extension-toolkit/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { BudgetToolModule } from '../../../budget-tool/src/app/app.module';
 
 const routes: Routes = [
   // { path: '', redirectTo: '/budget', pathMatch: 'full' },
+  // support embed of budget tool app
   {
     path: 'budget',
     loadChildren: () =>
@@ -19,6 +20,7 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(
       routes
+      // CC Note - strategy fails with embedded sub-apps (not sure why), could add custom strategy
       // , { preloadingStrategy: PreloadAllModules }
     ),
     BudgetToolModule.forRoot()
