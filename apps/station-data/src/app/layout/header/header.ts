c386c69266fc48f57e833be15c5f1073e6be946b
Chris\Laptop <chris.m.clarke@live.co.uk>
2019-07-06 20:59:21 -0700

    rename station data to dashboard, minor style changes

--- apps/station-data/src/app/layout/header/header.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'station-data-header',
-  templateUrl: './header.html'
-})
-export class StationDataHeader {}
--- apps/station-data/src/app/layout/sidebar/sidebar.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'station-data-sidebar',
-  templateUrl: './sidebar.html',
-  styleUrls: ['./sidebar.scss']
-})
-export class StationDataSidebar {}
