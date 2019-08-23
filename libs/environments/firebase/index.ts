f5122d3bbe63db9f44d42c7417469688cb871f75
Chris <chris.m.clarke@live.co.uk>
2019-08-23 17:38:50 +0100

    Revert "change firebase config for picsa-apps"
    This reverts commit 2288fc0da61b5c3d970ffc67e12a76d6f81edc84.

--- libs/environments/firebase/index.ts
+++ libs/environments/firebase/index.ts
@@ -1,11 +1,11 @@
 const DEV = {
-  apiKey: 'AIzaSyBuEu95p5xS2PHRH3miUEm9I3SZ2ufCHV4',
-  authDomain: 'picsa-apps.firebaseapp.com',
-  databaseURL: 'https://picsa-apps.firebaseio.com',
-  projectId: 'picsa-apps',
-  storageBucket: 'picsa-apps.appspot.com',
-  messagingSenderId: '78373580660',
-  appId: '1:78373580660:web:a16e594eb01028dc'
+  apiKey: 'AIzaSyCHzsaVc4TuG3QMFjI_SKP1Px-E5QRglcM',
+  authDomain: 'extension-toolkit.firebaseapp.com',
+  databaseURL: 'https://extension-toolkit.firebaseio.com',
+  projectId: 'extension-toolkit',
+  storageBucket: 'extension-toolkit.appspot.com',
+  messagingSenderId: '249750594240',
+  appId: '1:249750594240:web:85afd34173faddcc'
 };
 
 // NOT COMMITTED
