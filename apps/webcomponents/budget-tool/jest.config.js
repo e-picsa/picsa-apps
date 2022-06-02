1dfa37455fe68b992f5233ccedaadfd13bfff809
chrismclarke <chris.m.clarke@live.co.uk>
2022-06-02 09:36:30 -0700

    chore: core updates

--- apps/webcomponents/budget-tool/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'budget-tool',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/budget-tool',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
