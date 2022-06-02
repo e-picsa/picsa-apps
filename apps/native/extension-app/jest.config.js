5449e13d9819b1889181c09cb9590431eeab2410
chrismclarke <chris.m.clarke@live.co.uk>
2022-06-02 09:20:14 -0700

    chore: remove legacy schematics

--- apps/native/extension-app/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'extension-toolkit',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/extension-toolkit',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
