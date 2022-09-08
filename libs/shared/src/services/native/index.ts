ad85e74f73321d37e754817c6f5c414f02a81cb2
Chris <chris.m.clarke@live.co.uk>
2022-09-08 22:32:21 +0100

    feat: native storage service

--- libs/shared/src/services/native/index.ts
+++ libs/shared/src/services/native/index.ts
@@ -1,5 +1,7 @@
 // NOTE - to use these service must import PicsaNativeModule in app root with forRoot call
 
+export * from './eventService';
 export * from './file-service';
+export * from './storage-service';
 export * from './network';
 export * from './print';
