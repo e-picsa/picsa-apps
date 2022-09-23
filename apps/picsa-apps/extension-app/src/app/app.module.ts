807057536f1f2df5f15e49006f23c945671b11c5
Chris\Laptop <chris.m.clarke@live.co.uk>
2022-09-23 09:41:25 +0200

    chore: code tidying

--- apps/picsa-apps/extension-app/src/app/app.module.ts
+++ apps/picsa-apps/extension-app/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -15,10 +14,9 @@ import {
 import { IonicModule } from '@ionic/angular';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 //Sentry imports
-import * as Sentry from '@sentry/capacitor'
-import * as SentryAngular from '@sentry/angular'
+import * as Sentry from '@sentry/capacitor';
+import * as SentryAngular from '@sentry/angular';
 import { SENTRY_CONFIG } from '@picsa/environments/src/sentry';
-
 
 // NOTE - climate slider requires import into main modules
 import { MatSliderModule } from '@angular/material/slider';
@@ -34,34 +32,18 @@ export class SentryErrorHandler implements ErrorHandler {
     throw error;
   }
 }
-/**************************************************************
- *  Sentry error handler
- * ***************************************************************/
-// import * as Sentry from 'sentry-cordova';
-// import { SENTRY_CONFIG } from '@picsa/environments/sentry';
-// Sentry.init(SENTRY_CONFIG);
-// export class SentryIonicErrorHandler extends ErrorHandler {
-//   override handleError(error) {
-//     super.handleError(error);
-//     try {
-//       Sentry.captureException(error.originalError || error);
-//     } catch (e) {
-//       console.error(e);
-//     }
-//   }
-// }
-
-
 
 @NgModule({
   declarations: [AppComponent],
   providers: [
-     { 
-      provide: ErrorHandler, 
-      useValue: SentryAngular.createErrorHandler({logErrors:true,showDialog:false}),
-      useClass: SentryErrorHandler 
+    {
+      provide: ErrorHandler,
+      useValue: SentryAngular.createErrorHandler({
+        logErrors: true,
+        showDialog: false,
+      }),
+      useClass: SentryErrorHandler,
     },
-
   ],
   imports: [
     BrowserModule,
--- libs/environments/src/sentry.ts
+++ libs/environments/src/sentry.ts
@@ -2,5 +2,5 @@ import { APP_VERSION } from './version';
 // used to configure sentry and link to sentry releases
 export const SENTRY_CONFIG = {
   dsn: 'https://79ac03b265304bf3a8f50ec8fd34b59a@o1420740.ingest.sentry.io/6765888',
-  release: `picsa-app@${APP_VERSION.number}`
+  release: `picsa-app@${APP_VERSION.number}`,
 };
