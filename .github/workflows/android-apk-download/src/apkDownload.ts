93c8153101afebf298270a1d752056ddfc98ecac
chrismclarke <chris.m.clarke@live.co.uk>
2022-10-14 13:22:39 -0700

    chore: ci

--- .github/workflows/android-apk-download/src/apkDownload.ts
+++ .github/workflows/android-apk-download/src/apkDownload.ts
@@ -4,6 +4,7 @@ import { androidpublisher_v3 } from '@googleapis/androidpublisher';
 import AndroidPublisher = androidpublisher_v3.Androidpublisher;
 import { Compute } from 'google-auth-library/build/src/auth/computeclient';
 import { JSONClient } from 'google-auth-library/build/src/auth/googleauth';
+import { createWriteStream, statSync, writeFileSync } from 'fs';
 
 const androidPublisher: AndroidPublisher = google.androidpublisher('v3');
 
@@ -16,29 +17,59 @@ export interface EditOptions {
 export async function apkDownload(
   options: EditOptions
 ): Promise<string | void> {
+  const { auth, versionCode } = options;
   // Get a list of available apks for version code
-  const res = await androidPublisher.generatedapks.list({
-    auth: options.auth,
+  const listRes = await androidPublisher.generatedapks.list({
+    auth,
     packageName: options.applicationId,
-    versionCode: options.versionCode,
+    versionCode,
   });
-  const apksByKey = res.data.generatedApks || [];
+  const apksByKey = listRes.data.generatedApks || [];
   console.log(`apks found for ${apksByKey.length} keys`);
   if (apksByKey[0]) {
     // Get id for universal apk and download
     const { generatedUniversalApk } = apksByKey[0];
     console.log(generatedUniversalApk);
     if (generatedUniversalApk) {
-      const dlRes = await androidPublisher.generatedapks.download({
-        auth: options.auth,
-        packageName: options.applicationId,
-        versionCode: options.versionCode,
-        downloadId: generatedUniversalApk.downloadId as string,
-        alt: 'media',
-      });
-      console.log(dlRes.data);
+      await downloadApk(options, generatedUniversalApk.downloadId as string);
+      return;
     }
   }
 
-  return res.statusText;
+  return;
+}
+
+function downloadApk(options: EditOptions, downloadId: string) {
+  const { auth, versionCode } = options;
+  const fileName = `${versionCode}.apk`;
+  const filePath = `./${fileName}`;
+  return new Promise((resolve, reject) => {
+    androidPublisher.generatedapks.download(
+      {
+        auth,
+        packageName: options.applicationId,
+        versionCode,
+        downloadId,
+        alt: 'media',
+      },
+      { responseType: 'stream' },
+      (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          res!.data
+            .on('end', function () {
+              console.log('Done');
+              statSync(filePath);
+              resolve(true);
+            })
+            .on('error', function (err) {
+              console.log('Error during download', err);
+              reject(err);
+            })
+            .pipe(createWriteStream(filePath));
+        }
+      }
+    );
+  });
 }
