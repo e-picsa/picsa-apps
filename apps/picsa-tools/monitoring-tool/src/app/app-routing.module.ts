3b6d393d9146c764562c1229011cb99762451c01
chrismclarke <chris.m.clarke@live.co.uk>
2022-11-02 08:20:05 -0700

    feat: add monitoring tool workspace

---
+++ apps/picsa-tools/monitoring-tool/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+export const ROUTES_COMMON: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomeModule),
+    title: 'Home',
+  },
+];
+/** Routes only registered in standalone mode */
+const ROUTES_STANDALONE: Routes = [{ path: '**', redirectTo: '' }];
+
+/*******************************************************************
+ *  Standalone Version
+ ******************************************************************/
+@NgModule({
+  imports: [
+    RouterModule.forRoot([...ROUTES_COMMON, ...ROUTES_STANDALONE], {
+      preloadingStrategy: PreloadAllModules,
+    }),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ apps/picsa-tools/monitoring-tool/tailwind.config.js
@@ -0,0 +1,16 @@
+const { createGlobPatternsForDependencies } = require('@nrwl/angular/tailwind');
+const { join } = require('path');
+
+module.exports = {
+  content: [
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // Optional - can include if planning on developing own form theme
+    // require('@tailwindcss/forms')
+  ],
+};
