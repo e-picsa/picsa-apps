afe339dadbbb5de90e14bac16e11abaf630107d3
chrismclarke <chris.m.clarke@live.co.uk>
2022-11-05 23:01:36 -0700

    chore: update preview target and fix build

--- libs/webcomponents-ngx/src/lib/generated/components.ts
+++ libs/webcomponents-ngx/src/lib/generated/components.ts
@@ -1,28 +1,24 @@
 /* tslint:disable */
 /* auto-generated angular directive proxies */
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  ElementRef,
-  EventEmitter,
-  NgZone,
-} from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, EventEmitter, NgZone } from '@angular/core';
 import { ProxyCmp, proxyOutputs } from './angular-component-lib/utils';
 
 import { Components } from '@picsa/webcomponents';
+
+
+
 
 export declare interface EnketoWebform extends Components.EnketoWebform {}
 
 @ProxyCmp({
   defineCustomElementFn: undefined,
-  inputs: ['form', 'model'],
+  inputs: ['form', 'model']
 })
 @Component({
   selector: 'enketo-webform',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: '<ng-content></ng-content>',
-  inputs: ['form', 'model'],
+  inputs: ['form', 'model']
 })
 export class EnketoWebform {
   protected el: HTMLElement;
