2abd8de50ecd0a4ddf638cd9b1a146d7173033aa
Chris <chris.m.clarke@live.co.uk>
2023-03-10 13:34:11 +0000

    chore: remove welcome component

--- apps/picsa-tools/crop-probability-tool/src/app/app.module.ts
+++ apps/picsa-tools/crop-probability-tool/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { CropProbabilityTableComponent } from './components/crop-probability-table/crop-probability-table.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, CropProbabilityTableComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
---
+++ apps/picsa-tools/crop-probability-tool/src/app/components/crop-probability-table/crop-probability-table.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'picsa-crop-probability-table',
+  templateUrl: './crop-probability-table.component.html',
+  styleUrls: ['./crop-probability-table.component.scss'],
+})
+export class CropProbabilityTableComponent {}
