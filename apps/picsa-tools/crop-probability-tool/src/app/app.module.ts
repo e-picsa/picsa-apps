e3e1a971ab73681a5be33df86b3ac6f667240cb0
FaithDaka <faithbusigu8@gmail.com>
2023-03-29 22:18:24 +0300

    feat: table header declaration

--- apps/picsa-tools/crop-probability-tool/src/app/app.module.ts
+++ apps/picsa-tools/crop-probability-tool/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { CropProbabilityTableComponent } from './components/crop-probability-table/crop-probability-table.component';
 import { CropProbabilityMaterialModule } from './components/material.module';
+import { CropProbabilityTableHeaderComponent } from './components/crop-probability-table-header/crop-probability-table-header.component';
 
 @NgModule({
-  declarations: [AppComponent, CropProbabilityTableComponent],
+  declarations: [AppComponent, CropProbabilityTableComponent, CropProbabilityTableHeaderComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
