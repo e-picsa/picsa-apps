b50b642e2aa08737e32f25464dcb95c90d85601d
FaithDaka <faithbusigu8@gmail.com>
2023-05-03 22:31:53 +0300

    feat: custom crop icons

--- apps/picsa-tools/crop-probability-tool/src/app/components/material.module.ts
+++ apps/picsa-tools/crop-probability-tool/src/app/components/material.module.ts
@@ -1,14 +1,38 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
+import { DomSanitizer } from '@angular/platform-browser';
 
-const MODULES = [MatButtonModule, MatInputModule, MatTableModule, MatFormFieldModule];
+const MODULES = [MatButtonModule, MatInputModule, MatTableModule, MatFormFieldModule, MatIconModule, MatCardModule];
 
 // use custom module to make it easier to control what is available through app
 @NgModule({
   imports: MODULES,
   exports: MODULES,
 })
-export class CropProbabilityMaterialModule {}
+export class CropProbabilityMaterialModule {
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
+    this.registerIcons();
+  }
+
+  registerIcons() {
+    const CROP_PROBABILITY_ICONS = {
+      beans: 'beans',
+      cow_peas:'cow_peas',
+      ground_nuts:'ground_nuts',
+      maize:'maize',
+      sorghum:'sorghum',
+      soya_beans:'soya_beans',
+      sun_flower:'sun_flower'
+    };
+    for (const [key, value] of Object.entries(CROP_PROBABILITY_ICONS)) {
+      const iconName = `picsa_crop_${key}`;
+      const iconUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(`assets/icons/mat-icons/${value}.svg`);
+      this.matIconRegistry.addSvgIcon(iconName, iconUrl);
+    }
+  }
+}
