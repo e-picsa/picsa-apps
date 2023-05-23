8c6158e0f2cfc324546be5c982707c9b3c02d043
Chris <chris.m.clarke@live.co.uk>
2023-05-23 09:15:52 +0100

    feat: add formdata, db client and cors demo edge functions

---
+++ apps/picsa-server/docker/volumes/functions/_shared/client.ts
@@ -0,0 +1,29 @@
+import { createClient, SupabaseClient } from 'https://esm.sh/@supabase/supabase-js@2.22.0';
+
+export { SupabaseClient };
+
+export const getClient = (req: Request) => {
+  // Create a Supabase client with the Auth context of the logged in user.
+  return createClient(
+    // Supabase API URL - env var exported by default.
+    Deno.env.get('SUPABASE_URL') ?? '',
+    // Supabase API ANON KEY - env var exported by default.
+    Deno.env.get('SUPABASE_ANON_KEY') ?? '',
+    // Create client with Auth context of the user that called the function.
+    // This way your row-level-security (RLS) policies are applied.
+    // { global: { headers: { Authorization: req.headers.get('Authorization')! } } }
+    {
+      db: {
+        schema: 'public',
+      },
+      auth: {
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: true,
+      },
+      global: {
+        headers: { 'x-my-custom-header': 'my-app-name', Authorization: req.headers.get('Authorization') || '' },
+      },
+    }
+  );
+};
