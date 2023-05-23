47c6be8573a3b21907477eff9c30fe9d3d93b2be
FaithDaka <faithbusigu8@gmail.com>
2023-05-23 18:22:42 +0300

    feat: display crop information by id

--- apps/picsa-tools/crop-probability-tool/src/app/components/crop-probability-table/crop-probability-table.component.ts
+++ apps/picsa-tools/crop-probability-tool/src/app/components/crop-probability-table/crop-probability-table.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Pipe, PipeTransform } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 
+import { CROP_PROBABILITY_ICONS } from '../../data/icons';
 import { CROP_MOCK_DATA } from '../../data/mock';
 
 export interface PeriodicElement {
@@ -10,6 +11,33 @@ export interface PeriodicElement {
   symbol: string;
 }
 
+export interface ICropInformation {
+  id: number;
+  station_name: string;
+  station_data: {
+    crop: string;
+    data: {
+      variety: string;
+      days: string;
+      water: string[];
+      probabilities: string[];
+    }[];
+  };
+  notes: string[];
+  dates: string[];
+  season_probabilites: string[]
+}
+@Pipe({
+  name: 'filter',
+  pure: false,
+})
+export class FilterPipe implements PipeTransform {
+  transform(items: any[], property: string, value: any): any[] {
+    if (!items) return [];
+    if (!value) return items;
+    return items.filter((item) => item[property] === value);
+  }
+}
 @Component({
   selector: 'picsa-crop-probability-table',
   templateUrl: './crop-probability-table.component.html',
@@ -18,11 +46,16 @@ export interface PeriodicElement {
 export class CropProbabilityTableComponent {
   displayedColumns: string[] = ['crop', 'variety', 'days', 'water', 'probabilities'];
   dataSource = new MatTableDataSource(CROP_MOCK_DATA);
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
+  cropIcons = CROP_PROBABILITY_ICONS;
+  stations = CROP_MOCK_DATA;
+  stationId = 0;
+
   filterbyImage(title: string) {
     this.dataSource.filter = title.trim().toLowerCase();
+  }
+  onSelect(id: number) {
+    this.stationId = id;
+    this.dataSource.filterPredicate = (data: any, filter: string) => data.id === +this.stationId;
+    this.dataSource.filter = this.stationId.toString();
   }
 }
