759e7fe6f2ae375367e9ecb9a25503324bb5d16f
Chris <chris.m.clarke@live.co.uk>
2023-05-23 18:50:21 +0100

    chore: wip migrate scripts

---
+++ apps/picsa-server/scripts/migrate.ts
@@ -0,0 +1,9 @@
+export function migrate() {
+  // TODO
+  // https://github.com/supabase/postgres/tree/develop/migrations#supabasemigrations
+  // https://supabase.com/docs/guides/platform/migrating-and-upgrading-projects#backup-your-old-database
+  // https://github.com/amacneil/dbmate
+  // Could likely just be configured with docker-compose (use script to populate)
+  // https://github.com/supabase/postgres/blob/develop/migrations/docker-compose.yaml
+}
+migrate();
