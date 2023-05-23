1d1ac3bbd87f22ebb2534f09c34fef3ae25e0cd9
FaithDaka <faithbusigu8@gmail.com>
2023-05-23 18:24:43 +0300

    feat: icons files

---
+++ apps/picsa-tools/crop-probability-tool/src/app/data/icons.ts
@@ -0,0 +1,46 @@
+export const CROP_PROBABILITY_ICONS = [
+  {
+    name: 'maize',
+    icon_name: 'picsa_crop_maize',
+  },
+  {
+    name: 'cowpeas',
+    icon_name: 'picsa_crop_cow_peas',
+  },
+  {
+    name: 'beans',
+    icon_name: 'picsa_crop_beans',
+  },
+  {
+    name: 'soya',
+    icon_name: 'picsa_crop_soya_beans',
+  },
+  {
+    name: 'sorghum',
+    icon_name: 'picsa_crop_sorghum',
+  },
+  {
+    name: 'sunflower',
+    icon_name: 'picsa_crop_sun_flower',
+  },
+  {
+    name: 'rice',
+    icon_name: 'picsa_crop_rice',
+  },
+  {
+    name: 'groundnuts',
+    icon_name: 'picsa_crop_ground_nuts',
+  },
+  {
+    name: 'pigeon peas',
+    icon_name: 'picsa_crop_pigeon_peas',
+  },
+  {
+    name: 'sweet potatoes',
+    icon_name: 'picsa_crop_sweet_potatoes',
+  },
+  {
+    name: 'pumpkins',
+    icon_name: 'picsa_crop_pumpkins',
+  },
+];
