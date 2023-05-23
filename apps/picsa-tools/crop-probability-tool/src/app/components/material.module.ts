3eed2e8fcdd54d7daa7c81c339a444588098f474
FaithDaka <faithbusigu8@gmail.com>
2023-05-23 18:23:38 +0300

    feat: define select and option modules

--- apps/picsa-tools/crop-probability-tool/src/app/components/material.module.ts
+++ apps/picsa-tools/crop-probability-tool/src/app/components/material.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatOptionModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
 import { DomSanitizer } from '@angular/platform-browser';
 
-const MODULES = [MatButtonModule, MatInputModule, MatTableModule, MatFormFieldModule, MatIconModule, MatCardModule];
+const MODULES = [MatButtonModule, MatInputModule, MatTableModule, MatFormFieldModule, MatIconModule, MatCardModule, MatOptionModule, MatSelectModule];
 
 // use custom module to make it easier to control what is available through app
 @NgModule({
