2b835b599aa6fcca8392389aa83b585e8d76e981
Chris <chris.m.clarke@live.co.uk>
2023-07-07 09:50:39 +0100

    feat: add new crop data

--- apps/picsa-tools/crop-probability-tool/src/app/data/crops.ts
+++ apps/picsa-tools/crop-probability-tool/src/app/data/crops.ts
@@ -4,6 +4,7 @@ import { marker as translateMarker } from '@biesbjerg/ngx-translate-extract-mark
 export const CROPS_DATA = [
   { name: 'beans', label: translateMarker('Beans'), icon: 'assets/svgs/crops/beans.svg' },
   { name: 'cassava', label: translateMarker('Cassava'), icon: 'assets/svgs/crops/cassava.svg' },
+  { name: 'cotton', label: translateMarker('Cotton'), icon: 'assets/svgs/crops/cotton.svg' },
   { name: 'cowpeas', label: translateMarker('Cowpeas'), icon: 'assets/svgs/crops/cowpeas.svg' },
   { name: 'groundnuts', label: translateMarker('Groundnuts'), icon: 'assets/svgs/crops/groundnuts.svg' },
   { name: 'maize', label: translateMarker('Maize'), icon: 'assets/svgs/crops/maize.svg' },
@@ -12,8 +13,9 @@ export const CROPS_DATA = [
   { name: 'rice', label: translateMarker('Rice'), icon: 'assets/svgs/crops/rice.svg' },
   { name: 'sorghum', label: translateMarker('Sorghum'), icon: 'assets/svgs/crops/sorghum.svg' },
   { name: 'soya-beans', label: translateMarker('Soya Beans'), icon: 'assets/svgs/crops/soya-beans.svg' },
-  { name: 'sweet-potatoes', label: translateMarker('Sweet Potatoes'), icon: 'assets/svgs/crops/sweet-potatoes.svg' },
   { name: 'sunflower', label: translateMarker('Sunflower'), icon: 'assets/svgs/crops/sunflower.svg' },
+  { name: 'sweet-potatoes', label: translateMarker('Sweet Potatoes'), icon: 'assets/svgs/crops/sweet-potatoes.svg' },
+  { name: 'tobacco', label: translateMarker('Tobacco'), icon: 'assets/svgs/crops/tobacco.svg' },
 ] as const;
 
 // type-defintions extracted from hardcoded data
