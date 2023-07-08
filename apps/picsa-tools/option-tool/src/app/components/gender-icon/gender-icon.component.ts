9790c7c5bd4214b2bfcf3232d7f0d633227b11fd
chrismclarke <chris.m.clarke@live.co.uk>
2023-07-08 21:23:38 +0200

    feat: add gender icon component

---
+++ apps/picsa-tools/option-tool/src/app/components/gender-icon/gender-icon.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'picsa-gender-icon',
+  templateUrl: './gender-icon.component.html',
+  styleUrls: ['./gender-icon.component.scss'],
+})
+export class GenderIconComponent {
+  @Input() gender: 'male' | 'female';
+  @Input() showLabel = true;
+}
