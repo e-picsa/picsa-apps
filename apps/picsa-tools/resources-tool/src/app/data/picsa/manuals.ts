f31f32298094fe82c80efac90acc981cc0aff25a
chrismclarke <chris.m.clarke@live.co.uk>
2023-07-21 11:21:49 +0200

    feat: add chichewa farmer manual resource

--- apps/picsa-tools/resources-tool/src/app/data/picsa/manuals.ts
+++ apps/picsa-tools/resources-tool/src/app/data/picsa/manuals.ts
@@ -1,6 +1,11 @@
 import { IResourceCollection, IResourceFile } from '../../models';
 
-type IPicsaManualId = 'picsa_manual' | 'picsa_manual_chichewa' | 'picsa_manual_tumbuka' | 'picsa_manual_tajik';
+type IPicsaManualId =
+  | 'picsa_manual'
+  | 'picsa_manual_chichewa'
+  | 'picsa_manual_chichewa_farmer'
+  | 'picsa_manual_tumbuka'
+  | 'picsa_manual_tajik';
 
 /**
  * List of manuals available both within manual tool and resource collection
@@ -11,42 +16,60 @@ export const PICSA_MANUAL_RESOURCES: { [id in IPicsaManualId]: IResourceFile } =
     _key: 'picsa_manual',
     _created: '2019-09-25T10:00:01.000Z',
     _modified: '2019-09-25T11:00:01.000Z',
-    title: 'PICSA Manual (English)',
-    language: 'eng',
+    title: 'PICSA Manual',
+    language: 'english',
     mimetype: 'application/pdf',
     description: '',
     subtitle: '',
     filename: 'picsa-field-manual-en.pdf',
     type: 'file',
     url: 'https://firebasestorage.googleapis.com/v0/b/picsa-apps.appspot.com/o/picsa%2Fmanuals%2Fpicsa-manual.pdf?alt=media&token=8d5c21da-2252-44b7-8709-a3f12a593c2f',
+    keywords: ['manual', 'extension'],
   },
   picsa_manual_chichewa: {
     _key: 'picsa_manual_chichewa',
     _created: '2019-09-25T10:00:02.000Z',
     _modified: '2019-09-27T15:00:03.000Z',
-    title: 'Buku la PICSA (Chichewa)',
+    title: 'Buku la PICSA (Extension)',
     mimetype: 'application/pdf',
-    language: 'nya',
+    language: 'chichewa',
     description: '',
-    subtitle: 'Chichewa',
+    subtitle: 'Extension',
     filename: 'picsa-field-manual-chichewa.pdf',
     type: 'file',
     url: 'https://firebasestorage.googleapis.com/v0/b/picsa-apps.appspot.com/o/picsa%2Fmanuals%2Fpicsa-manual-chichewa.pdf?alt=media&token=d03031db-9e83-4a0a-a5be-7c3d1d42f98b',
     appCountries: ['mw', 'zm'],
+    keywords: ['manual', 'extension'],
+  },
+  picsa_manual_chichewa_farmer: {
+    _key: 'picsa_manual_chichewa_farmer',
+    _created: '2019-09-25T10:00:02.000Z',
+    _modified: '2019-09-27T15:00:03.000Z',
+    title: 'Buku la PICSA (Farmer)',
+    mimetype: 'application/pdf',
+    language: 'chichewa',
+    description: '',
+    subtitle: '',
+    filename: 'picsa-field-manual-chichewa.pdf',
+    type: 'file',
+    url: 'https://firebasestorage.googleapis.com/v0/b/picsa-apps.appspot.com/o/picsa%2Fmanuals%2Fpicsa-manual-chichewa-farmer.pdf?alt=media&token=c43a2f2a-ae68-49b9-bb15-31ad788c48d5',
+    appCountries: ['mw', 'zm'],
+    keywords: ['manual', 'farmer'],
   },
   picsa_manual_tumbuka: {
     _key: 'picsa_manual_tumbuka',
     _created: '2019-09-25T10:00:03.000Z',
     _modified: '2019-09-27T15:00:05.000Z',
-    title: 'Buku la PICSA (Tumbuka)',
+    title: 'Buku la PICSA',
     mimetype: 'application/pdf',
     description: '',
-    language: 'tum',
-    subtitle: 'Tumbuka',
+    language: 'tumbuka',
+    subtitle: '',
     filename: 'picsa-field-manual-tumbuka.pdf',
     type: 'file',
     url: 'https://firebasestorage.googleapis.com/v0/b/picsa-apps.appspot.com/o/picsa%2Fmanuals%2Fpicsa-manual-tumbuka.pdf?alt=media&token=ccaa5881-2200-48de-8799-69179b8109e0',
     appCountries: ['mw', 'zm'],
+    keywords: ['manual', 'extension'],
   },
   picsa_manual_tajik: {
     _key: 'picsa_manual_tajik',
@@ -55,12 +78,13 @@ export const PICSA_MANUAL_RESOURCES: { [id in IPicsaManualId]: IResourceFile } =
     title: 'ПИКСА (Забони тоҷикӣ)',
     mimetype: 'application/pdf',
     description: '',
-    language: 'tj',
+    language: 'tajik',
     subtitle: '',
     filename: 'picsa_manual_tajik.pdf',
     type: 'file',
     url: 'https://firebasestorage.googleapis.com/v0/b/picsa-apps.appspot.com/o/picsa%2Fmanuals%2Fpicsa_manual_tajik.pdf?alt=media&token=b8669e3c-9eae-4443-b743-6b1881990d81',
     appCountries: ['tj'],
+    keywords: ['manual', 'extension'],
   },
 };
 
