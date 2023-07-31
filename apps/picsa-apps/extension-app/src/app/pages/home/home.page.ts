8bae5adb980b27204d50f2609949505f62fd9804
FaithDaka <faithbusigu8@gmail.com>
2023-07-31 12:12:39 +0300

    refactor implementation of tour steps

--- apps/picsa-apps/extension-app/src/app/pages/home/home.page.ts
+++ apps/picsa-apps/extension-app/src/app/pages/home/home.page.ts
@@ -1,5 +1,5 @@
 import { DomPortal } from '@angular/cdk/portal';
-import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { marker as translateMarker } from '@biesbjerg/ngx-translate-extract-marker';
 import { PicsaCommonComponentsService } from '@picsa/components/src';
@@ -13,6 +13,12 @@ interface IPageLink {
   icon: string;
   url: string;
   element: string;
+}
+
+interface ITourStep {
+  element: string;
+  intro: string;
+  position?: string;
 }
 
 const PAGE_LINKS: IPageLink[] = [
@@ -72,6 +78,43 @@ const PAGE_LINKS: IPageLink[] = [
   // }
 ];
 
+const TOUR_STEPS: ITourStep[] = [
+  {
+    element: '#element-1',
+    intro: 'This is the EPICSA Manual. A step-by-step guide to using PICSA with farmers.',
+  },
+  {
+    element: '#element-7',
+    intro:
+      'This is the options tool that supports farmers to consider a range of options aimed at increasing production, income and resilience.',
+  },
+  {
+    element: '#element-2',
+    intro: 'This is where you’ll find helpful resources that will guide your work.',
+  },
+  {
+    element: '#element-3',
+    intro:
+      'In here, field staff can make records of visits with farmers and provide data on everything they monitored.',
+  },
+  {
+    element: '#element-4',
+    intro:
+      'This is the climate tool which provides automatically updated, locally specific climate information graphs. A way for you to analyse the changing climate in your region.',
+  },
+  {
+    element: '#element-5',
+    intro:
+      'This budget tool is fully interactive and provides a way for farmers to make extensive budgets with respect to different options in their agro-businesses.',
+  },
+  {
+    position: 'left',
+    element: '#element-6',
+    intro:
+      'This is the crop-probability tool. It provides immediate calculations on which crops and varieties have the best chance to succeed, according to regions.',
+  },
+];
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -100,6 +143,7 @@ export class HomePage implements OnDestroy, AfterViewInit {
     this.componentsService.patchHeader({ endContent: undefined });
     this.userEventSubscription.unsubscribe();
   }
+  
   ngAfterViewInit() {
     this.componentsService.patchHeader({
       endContent: new DomPortal(this.headerContent),
@@ -122,19 +166,9 @@ export class HomePage implements OnDestroy, AfterViewInit {
             intro: 'Click Next if you would like to take a tour of the app or click the <b> ’x’ </b>to close.',
           },
           {
-            element: '#element-1',
-            intro: 'This is the EPICSA Manual. A step-by-step guide to using PICSA with farmers.',
-          },
-          {
             element: '#element-7',
             intro:
               'This is the options tool that supports farmers to consider a range of options aimed at increasing production, income and resilience.',
-          },
-          {
-            position: 'left',
-            element: '#element-6',
-            intro:
-              'This is the crop-probability tool. It provides immediate calculations on which crops and varieties have the best chance to succeed, according to regions.',
           },
           {
             element: '#element-2',
@@ -154,6 +188,12 @@ export class HomePage implements OnDestroy, AfterViewInit {
             element: '#element-5',
             intro:
               'This budget tool is fully interactive and provides a way for farmers to make extensive budgets with respect to different options in their agro-businesses.',
+          },
+          {
+            position: 'left',
+            element: '#element-6',
+            intro:
+              'This is the crop-probability tool. It provides immediate calculations on which crops and varieties have the best chance to succeed, according to regions.',
           },
         ],
       })
@@ -167,42 +207,7 @@ export class HomePage implements OnDestroy, AfterViewInit {
         hidePrev: true,
         dontShowAgain: true,
         tooltipClass: 'customTooltip',
-        steps: [
-          {
-            element: '#element-1',
-            intro: 'This is the EPICSA Manual. A step-by-step guide to using PICSA with farmers.',
-          },
-          {
-            element: '#element-7',
-            intro:
-              'This is the options tool that supports farmers to consider a range of options aimed at increasing production, income and resilience.',
-          },
-          {
-            position: 'left',
-            element: '#element-6',
-            intro:
-              'This is the crop-probability tool. It provides immediate calculations on which crops and varieties have the best chance to succeed, according to regions.',
-          },
-          {
-            element: '#element-2',
-            intro: 'This is where you’ll find helpful resources that will guide your work.',
-          },
-          {
-            element: '#element-3',
-            intro:
-              'In here, field staff can make records of visits with farmers and provide data on everything they monitored.',
-          },
-          {
-            element: '#element-4',
-            intro:
-              'This is the climate tool which provides automatically updated, locally specific climate information graphs. A way for you to analyse the changing climate in your region.',
-          },
-          {
-            element: '#element-5',
-            intro:
-              'This budget tool is fully interactive and provides a way for farmers to make extensive budgets with respect to different options in their agro-businesses.',
-          },
-        ],
+        steps: TOUR_STEPS
       })
       .start();
   }
