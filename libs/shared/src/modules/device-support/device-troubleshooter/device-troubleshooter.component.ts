bb8fe9e6185c21311aee4e823efc8bdeec1c3f9e
chrismclarke <chris.m.clarke@live.co.uk>
2023-09-15 20:45:35 -0700

    chore: revert device support service changes

--- libs/shared/src/modules/device-support/device-troubleshooter/device-troubleshooter.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'picsa-device-troubleshooter',
-  templateUrl: './device-troubleshooter.component.html',
-  styleUrls: ['./device-troubleshooter.component.scss'],
-})
-export class DeviceTroubleshooterComponent {}
