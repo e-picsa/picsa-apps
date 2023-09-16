486ed6957e18e6483e3828c24ac1e8c8a94bdedc
chrismclarke <chris.m.clarke@live.co.uk>
2023-09-15 20:55:18 -0700

    chore: code tidying

--- libs/shared/src/modules/index.ts
+++ libs/shared/src/modules/index.ts
@@ -1,7 +1,6 @@
 import { PicsaDb_V2_Module } from './database_v2/db.module';
 import { PicsaDbModule } from './db.module';
 import { PicsaDeepLinksModule } from './deep-links/deep-links.module';
-import { PicsaDeviceSupportModule } from './device-support';
 import { PicsaNativeModule } from './native';
 import { PicsaTranslateModule, PicsaTranslateService } from './translate';
 
