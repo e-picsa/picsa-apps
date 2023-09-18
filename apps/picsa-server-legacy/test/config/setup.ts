1e308beadac28e40a4cc9fc3aba5598f44daa362
chrismclarke <chris.m.clarke@live.co.uk>
2023-09-17 20:20:10 -0700

    chore: nx migrations

--- apps/picsa-server-legacy/test/config/setup.ts
+++ apps/picsa-server-legacy/test/config/setup.ts
@@ -12,7 +12,7 @@ tsConfigRegister({
   paths: compilerOptions.paths,
 });
 
-// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+// eslint-disable-next-line @nx/enforce-module-boundaries
 import { populateEnv } from '@picsa/scripts';
 import { commandSync } from 'execa';
 import path from 'path';
--- libs/webcomponents/jest.config.ts
+++ libs/webcomponents/jest.config.ts
@@ -5,19 +5,20 @@
 export default {
   displayName: 'webcomponents',
   // HACK
-  // preset: require('@nrwl/jest/preset').default,
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      // ignore compiler warnings
-      // https://huafu.github.io/ts-jest/user/config/diagnostics
-      diagnostics: {
-        warnOnly: true,
-      },
-    },
-  },
+  // preset: require('@nx/jest/preset').default,
+  globals: {},
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest',
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        // ignore compiler warnings
+        // https://huafu.github.io/ts-jest/user/config/diagnostics
+        diagnostics: {
+          warnOnly: true,
+        },
+      },
+    ],
   },
   // moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/i18n',
@@ -26,4 +27,14 @@ export default {
   moduleNameMapper: {
     // '/src/(.*)': '<rootDir>/src/$1',
   },
+  /* TODO: Update to latest Jest snapshotFormat
+   * By default Nx has kept the older style of Jest Snapshot formats
+   * to prevent breaking of any existing tests with snapshots.
+   * It's recommend you update to the latest format.
+   * You can do this by removing snapshotFormat property
+   * and running tests with --update-snapshot flag.
+   * Example: From within the project directory, run "nx test --update-snapshot"
+   * More info: https://jestjs.io/docs/upgrading-to-jest29#snapshot-format
+   */
+  snapshotFormat: { escapeString: true, printBasicPrototype: true },
 };
