2651cb37bcbb62a9f1bf4887b7b4cf3304bc23c1
chrismclarke <chris.m.clarke@live.co.uk>
2023-09-24 09:29:55 -0700

    feat: seasonal calendar tool scaffolding

---
+++ apps/picsa-tools/seasonal-calendar-tool/src/app/app.module.embedded.ts
@@ -0,0 +1,49 @@
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { PicsaTranslateService } from '@picsa/shared/modules';
+import { registerEmbeddedRoutes } from '@picsa/utils';
+
+import { AppComponentEmbedded } from './app.component';
+import { APP_COMMON_IMPORTS } from './app.module';
+import { ROUTES_COMMON } from './app-routing.module';
+
+export class EmbeddedConfig {
+  /** Path app routed through, e.g. 'budget' */
+  urlPrefix: string;
+}
+
+/*******************************************************************
+ *  Routes
+ ******************************************************************/
+@NgModule({
+  imports: [RouterModule.forChild([])],
+})
+export class EmbeddedRoutingModule {
+  constructor(router: Router, embeddedConfig: EmbeddedConfig) {
+    registerEmbeddedRoutes(ROUTES_COMMON, router, embeddedConfig.urlPrefix);
+  }
+}
+
+/*******************************************************************
+ * Module
+ ******************************************************************/
+@NgModule({
+  declarations: [AppComponentEmbedded],
+  imports: [...APP_COMMON_IMPORTS, EmbeddedRoutingModule],
+  bootstrap: [AppComponentEmbedded],
+})
+export class BaseModule {
+  // ensure translate has been initiated
+  constructor(public translate: PicsaTranslateService) {}
+}
+
+/** Use to import directly into another app via lazy-loading */
+@NgModule()
+export class SeasonalCalendarToolModule {
+  static forRoot(config: EmbeddedConfig): ModuleWithProviders<BaseModule> {
+    return {
+      ngModule: BaseModule,
+      providers: [PicsaTranslateService, { provide: EmbeddedConfig, useValue: config }],
+    };
+  }
+}
---
+++ apps/picsa-tools/seasonal-calendar-tool/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { PicsaCommonComponentsModule } from '@picsa/components';
+import { PicsaDb_V2_Module, PicsaTranslateModule, PicsaTranslateService } from '@picsa/shared/modules';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SeasonalCalendarMaterialModule } from './components/material.module';
+
+/** Core imports only required when running standalone */
+const StandaloneImports = [
+  AppRoutingModule,
+  BrowserModule,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  PicsaTranslateModule.forRoot(),
+  PicsaDb_V2_Module.forRoot(),
+];
+
+/** Common imports used in both standalone and embedded formats */
+export const APP_COMMON_IMPORTS = [
+  HttpClientModule,
+  SeasonalCalendarMaterialModule,
+  PicsaTranslateModule,
+  PicsaDb_V2_Module,
+  PicsaCommonComponentsModule,
+];
+
+/*******************************************************************
+ *  Standalone Version
+ ******************************************************************/
+@NgModule({
+  declarations: [AppComponent],
+  imports: [...StandaloneImports, ...APP_COMMON_IMPORTS],
+  bootstrap: [AppComponent],
+  schemas: [],
+})
+export class AppModule {
+  // ensure translate service initialised
+  constructor(public translate: PicsaTranslateService) {}
+}
