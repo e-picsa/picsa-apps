ee468f86b1f50764d5c79a8568b106475dcc58a3
chrismclarke <chris.m.clarke@live.co.uk>
2023-09-27 17:04:27 -0700

    chore: remove deprecated components and services

--- libs/shared/src/modules/native.ts
+++ libs/shared/src/modules/native.ts
@@ -1,14 +1,8 @@
-import {
-  ModuleWithProviders,
-  NgModule,
-  Optional,
-  SkipSelf,
-} from '@angular/core';
+import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 import { File } from '@awesome-cordova-plugins/file/ngx';
 import { FileOpener } from '@awesome-cordova-plugins/file-opener/ngx';
 import { SocialSharing } from '@awesome-cordova-plugins/social-sharing/ngx';
 
-import { PicsaFileService } from '../services/native/file-service';
 import { NetworkProvider } from '../services/native/network';
 import { PrintProvider } from '../services/native/print';
 
@@ -18,22 +12,13 @@ import { PrintProvider } from '../services/native/print';
 export class PicsaNativeModule {
   constructor(@Optional() @SkipSelf() parentModule: PicsaNativeModule) {
     if (parentModule) {
-      throw new Error(
-        'PicsaNativeServices is already loaded. Import it in the AppModule only'
-      );
+      throw new Error('PicsaNativeServices is already loaded. Import it in the AppModule only');
     }
   }
   static forRoot(): ModuleWithProviders<PicsaNativeModule> {
     return {
       ngModule: PicsaNativeModule,
-      providers: [
-        PrintProvider,
-        NetworkProvider,
-        PicsaFileService,
-        SocialSharing,
-        File,
-        FileOpener,
-      ],
+      providers: [PrintProvider, NetworkProvider, SocialSharing, File, FileOpener],
     };
   }
 }
