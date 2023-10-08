c5f512f357763ab7522d5a60e4be74bdff1c6102
khalifan-kfan <khalifanmuwonge@gmail.com>
2023-10-08 23:22:01 +0300

    connect to real data

--- apps/picsa-tools/seasonal-calendar-tool/src/app/pages/calender-table/calendar-table.component.ts
+++ apps/picsa-tools/seasonal-calendar-tool/src/app/pages/calender-table/calendar-table.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, Router } from '@angular/router'
 
 import { DataService } from './../../services/calender.data.service';
 
@@ -11,62 +11,37 @@ import { DataService } from './../../services/calender.data.service';
 })
 
 export class CalendarTableComponent implements OnInit  {
-  calendarData: any | null = null;
-
-   sampleData:SampleData = {
-    name: 'Sample Calendar',
-    timeAndWeather: [
-      { month: 'January', weather: 'Sunny' },
-      { month: 'February', weather: 'Rainy' },
-    ],
-    crops: [
-      {
-        name: 'Maize',
-        months: [
-          { month: 'January', activities: ['Planting', 'Weeding'] },
-          { month: 'February', activities: ['Weeding', 'Harvesting'] },
-        ],
-        extraInformation: 'Extra data for Maize',
-      },
-      {
-        name: 'Beans',
-        months: [
-          { month: 'January', activities: ['Planting', 'Preparation'] },
-          { month: 'February', activities: ['Weeding', 'Harvesting'] },
-        ],
-        extraInformation: 'Extra data for Beans',
-      },
-    ],
-  }; 
-  
+  calendarData: CalendarData | null = null;
   months: string[] = [
     'January', 'February', 'March', 'April',
     'May', 'June', 'July', 'August',
     'September',
   ];
-  constructor(private route: ActivatedRoute, private dataService: DataService) {}
+  constructor(private route: ActivatedRoute, private dataService: DataService, private router: Router) {}
 
   ngOnInit() {
-    console.log(this.dataService)
+    this.route.paramMap.subscribe((params) => {
+      const calendarName = params.get('calendarName');
+      if(calendarName){
+      this.calendarData = this.dataService.calendars[calendarName]
+      if (!this.calendarData || Object.keys(this.calendarData).length === 0) {
+        this.router.navigate(['/seasonal-calendar']);
+      }
+      }
+    });
   }
-
  
   crops: string[] = ['Beans', 'Maize', 'Peas', 'Wheat', 'Barley']; 
 
   dataSource: MatTableDataSource<any>;
 
-  // This should contain your data from the service with appropriate structure
-  dataServiceData: any[] = [
-    { crop: 'Beans', January: ['Value1', 'Value5', 'value6', 'value7'], February: ['Value2', 'Value6']},
-    { crop: 'Maize', January: ['Value3'], February: ['Value4']},
-  ];
 
   getActivitiesForMonthAndCrop(monthName: string, crop: Crop): string {
     const selectedMonth = crop.months.find((month) => month.month === monthName);
     if (selectedMonth) {
-      return selectedMonth.activities.join(', '); // Display activities as a comma-separated string
+      return selectedMonth.activities.join(','); 
     } else {
-      return ''; // Return an empty string if no activities are found
+      return ''; 
     }
   }
   
@@ -88,8 +63,8 @@ interface Crop {
   extraInformation: string;
 }
 
-interface SampleData {
+interface CalendarData {
   name: string;
-  timeAndWeather: MonthData[];
+  timeAndConditions: MonthData[];
   crops: Crop[];
 }
