756999d58a7bc4e047c80802cc29bdfc05deacb4
chrismclarke <chris.m.clarke@live.co.uk>
2023-10-23 20:15:53 -0700

    chore: update translations

--- apps/picsa-tools/farmer-activity/src/app/pages/home/home.module.ts
+++ apps/picsa-tools/farmer-activity/src/app/pages/home/home.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { PicsaTranslateModule } from '@picsa/shared/modules';
 
 import { FarmerActivityComponentsModule } from '../../components/components.module';
 import { HomeComponent } from './home.component';
@@ -17,6 +18,7 @@ import { HomeComponent } from './home.component';
     ]),
     FormsModule,
     FarmerActivityComponentsModule,
+    PicsaTranslateModule,
   ],
   exports: [],
   declarations: [HomeComponent],
