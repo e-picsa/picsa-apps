1c272cefe7dd41193560c0048b3f4fc2232bc725
FaithDaka <faithbusigu8@gmail.com>
2023-10-25 22:51:20 +0300

    handle step two of guided tour

--- apps/picsa-tools/crop-probability-tool/src/app/components/station-select/station-select.component.ts
+++ apps/picsa-tools/crop-probability-tool/src/app/components/station-select/station-select.component.ts
@@ -1,8 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { TourService } from '@picsa/shared/services/core/tour.service';
 
 import { STATION_CROP_DATA } from '../../data/mock';
 import { IStationRouteQueryParams } from '../../models';
+import { CROP_PROBABILITY_TOUR_STEP_TWO } from '../../data/tour';
 
 @Component({
   selector: 'crop-probability-station-select',
@@ -14,11 +16,19 @@ export class CropProbabilityStationSelectComponent {
 
   @Input() selectedStationId?: string;
 
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  constructor(private router: Router, private route: ActivatedRoute, private tourService: TourService) {}
 
   /** When station changes update route query params so that parent can handle updates */
   public handleStationChange(stationId: string) {
     const queryParams: IStationRouteQueryParams = { stationId };
     this.router.navigate([], { relativeTo: this.route, queryParams });
+  }
+
+  // Trigger step two of tour after the dropdown has been clicked
+  public handleTourStepTwo() {
+    const trigger = localStorage.getItem('TourTrigger');
+    if (trigger === 'true') {
+      this.tourService.startTour(CROP_PROBABILITY_TOUR_STEP_TWO);
+    } 
   }
 }
