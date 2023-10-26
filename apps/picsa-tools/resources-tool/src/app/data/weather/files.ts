ca5a670189bfc76783a9b568649e30b2deb85a9b
chrismclarke <chris.m.clarke@live.co.uk>
2023-10-25 18:23:42 -0700

    content: zambia forecasts 2023

--- apps/picsa-tools/resources-tool/src/app/data/weather/files.ts
+++ apps/picsa-tools/resources-tool/src/app/data/weather/files.ts
@@ -2,11 +2,11 @@ import { IResourceFile } from '../../schemas';
 
 interface IDownscaledForecast extends IResourceFile {
   meta: {
-    locationId: string;
+    locationIds: string[];
   };
 }
 
-export const DOWNSCALED_FORECASTS: Record<string, IDownscaledForecast> = {
+const downscaledForecasts: Record<string, IDownscaledForecast> = {
   downscaled_forecast_2023_kasungu_en: {
     id: 'downscaled_forecast_2023_kasungu_en',
     description: '',
@@ -21,7 +21,7 @@ export const DOWNSCALED_FORECASTS: Record<string, IDownscaledForecast> = {
     filter: { countries: ['mw'] },
     language: 'en',
     meta: {
-      locationId: 'Malawi_Kasungu',
+      locationIds: ['Malawi_Kasungu'],
     },
   },
   downscaled_forecast_2023_kasungu_ny: {
@@ -38,7 +38,7 @@ export const DOWNSCALED_FORECASTS: Record<string, IDownscaledForecast> = {
     filter: { countries: ['mw'] },
     language: 'ny',
     meta: {
-      locationId: 'Malawi_Kasungu',
+      locationIds: ['Malawi_Kasungu'],
     },
   },
   downscaled_forecast_2023_nkhotakota_en: {
@@ -55,7 +55,7 @@ export const DOWNSCALED_FORECASTS: Record<string, IDownscaledForecast> = {
     filter: { countries: ['mw'] },
     language: 'en',
     meta: {
-      locationId: 'Malawi_Nkhotakota',
+      locationIds: ['Malawi_Nkhotakota'],
     },
   },
   downscaled_forecast_2023_nkhotakota_ny: {
@@ -72,7 +72,80 @@ export const DOWNSCALED_FORECASTS: Record<string, IDownscaledForecast> = {
     filter: { countries: ['mw'] },
     language: 'ny',
     meta: {
-      locationId: 'Malawi_Nkhotakota',
+      locationIds: ['Malawi_Nkhotakota'],
+    },
+  },
+  seasonal_forecast_2023_chipata: {
+    id: 'seasonal_forecast_2023_chipata',
+    description: '',
+    filename: 'Chipata_Seasonal_Forecast_2023_compressed.pdf',
+    mimetype: 'application/pdf',
+    title: 'Seasonal Forecast Chipata 2023',
+    type: 'file',
+    subtype: 'pdf',
+    url: 'https://firebasestorage.googleapis.com/v0/b/picsa-apps.appspot.com/o/weather%2FSeasonal%20Forecasts%2FChipata_Seasonal_Forecast_2023_compressed.pdf?alt=media&token=7b0eef18-7352-4340-b1dc-7d3d3887deb1',
+    size_kb: 264.5,
+    md5Checksum: 'e518fb9173765a36e7d82bb4ae3577d9',
+    filter: { countries: ['zm'] },
+    language: 'en',
+    meta: {
+      locationIds: ['Zambia_Chipata'],
+    },
+  },
+  seasonal_forecast_2023_petauke: {
+    id: 'seasonal_forecast_2023_petauke',
+    description: '',
+    filename: 'Petauke_Seasonal_Forecast_2023_compressed.pdf',
+    mimetype: 'application/pdf',
+    title: 'Seasonal Forecast Petauke 2023',
+    type: 'file',
+    subtype: 'pdf',
+    url: 'https://firebasestorage.googleapis.com/v0/b/picsa-apps.appspot.com/o/weather%2FSeasonal%20Forecasts%2FPetauke_Seasonal_Forecast_2023_compressed.pdf?alt=media&token=a6d05f21-a89d-45c5-bc19-cea3e549ba52',
+    size_kb: 259.5,
+    md5Checksum: '3cbbd07e607443e5a0c6bc0d302225f7',
+    filter: { countries: ['zm'] },
+    language: 'en',
+    meta: {
+      locationIds: ['Zambia_Petauke'],
     },
   },
 };
+
+const otherForecasts: Record<string, IDownscaledForecast> = {
+  rainfall_forecast_2023_zm: {
+    id: 'rainfall_forecast_2023_zm',
+    description: '',
+    filename: 'Zambia_Rainfall_Forecast_2023_2024_compressed.pdf',
+    mimetype: 'application/pdf',
+    title: 'RAINFALL FORECAST 2023/2024 SEASON',
+    type: 'file',
+    subtype: 'pdf',
+    url: 'https://firebasestorage.googleapis.com/v0/b/picsa-apps.appspot.com/o/weather%2FSeasonal%20Forecasts%2FZambia_Rainfall_Forecast_2023_2024_compressed.pdf?alt=media&token=21afe2eb-e809-42ac-a187-085ea8217cfd',
+    size_kb: 463.1,
+    md5Checksum: 'a7a9ac7d24d79177208dc2539eed07b8',
+    filter: { countries: ['zm'] },
+    language: 'en',
+    meta: {
+      locationIds: ['Zambia_Petauke', 'Zambia_Chipata'],
+    },
+  },
+  rainfall_forecast_2023_zm_ny: {
+    id: 'rainfall_forecast_2023_zm_ny',
+    description: '',
+    filename: 'Zambia_Chinyanja_Rainfall_Forecast_2023_2024.pdf',
+    mimetype: 'application/pdf',
+    title: 'KULOZERA ZA NYENGO YA MVULA YA CAKA CA 2023/2024',
+    type: 'file',
+    subtype: 'pdf',
+    url: 'https://firebasestorage.googleapis.com/v0/b/picsa-apps.appspot.com/o/weather%2FSeasonal%20Forecasts%2FZambia_Chinyanja_Rainfall_Forecast_2023_2024.pdf?alt=media&token=d700185e-b833-47b1-95a9-bc1e83d13bc9',
+    size_kb: 500.3,
+    md5Checksum: '76a01e3a99ff0abca5708bd020c3b5bf',
+    filter: { countries: ['zm'] },
+    language: 'ny',
+    meta: {
+      locationIds: ['Zambia_Petauke', 'Zambia_Chipata'],
+    },
+  },
+};
+
+export default { downscaledForecasts, otherForecasts };
