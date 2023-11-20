80ad94105553cbf42cea6067aab39be6401a9b5c
chrismclarke <chris.m.clarke@live.co.uk>
2023-11-19 16:13:26 -0800

    chore: deprecate legacy services

--- libs/shared/src/services/native/index.ts
+++ libs/shared/src/services/native/index.ts
@@ -1,5 +1,4 @@
 // NOTE - to use these service must import PicsaNativeModule in app root with forRoot call
 
-export * from './network';
 export * from './print';
 export * from './storage-service';
