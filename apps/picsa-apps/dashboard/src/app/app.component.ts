237ffcb32b555efb7a9692ce8b9f156b69730312
chrismclarke <chris.m.clarke@live.co.uk>
2023-11-23 22:08:51 -0800

    feat: add shared user notification service

--- apps/picsa-apps/dashboard/src/app/app.component.ts
+++ apps/picsa-apps/dashboard/src/app/app.component.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { PicsaNotificationService } from '@picsa/shared/services/core/notification.service';
+import { SupabaseService } from '@picsa/shared/services/core/supabase.service';
 
 import { DashboardMaterialModule } from './material.module';
+
+interface INavLink {
+  label: string;
+  href: string;
+  isActive?: boolean;
+}
 
 @Component({
   standalone: true,
@@ -10,6 +18,31 @@ import { DashboardMaterialModule } from './material.module';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'picsa-apps-dashboard';
+
+  navLinks: INavLink[] = [
+    {
+      label: 'Home',
+      href: '/',
+    },
+    {
+      label: 'Resources',
+      href: '/resources',
+    },
+  ];
+
+  constructor(private supabaseService: SupabaseService, private notificationService: PicsaNotificationService) {}
+
+  async ngAfterViewInit() {
+    await this.supabaseService.init();
+    const { data, error } = await this.supabaseService.auth.getUser();
+    if (error?.name === 'AuthRetryableFetchError') {
+      this.notificationService.showUserNotification({
+        icon: 'cloud_off',
+        message: 'Server Connection Failed',
+        buttonText: 'Dismiss',
+      });
+    }
+  }
 }
