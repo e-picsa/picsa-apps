6ca12138039397d433cf93ab2e7d5689e0d7f43d
Chris <chris.m.clarke@live.co.uk>
2023-12-21 11:53:09 +0000

    feat: station list and map

--- apps/picsa-apps/dashboard/src/app/modules/climate-data/pages/home/climate-data-home.component.ts
+++ apps/picsa-apps/dashboard/src/app/modules/climate-data/pages/home/climate-data-home.component.ts
@@ -2,21 +2,29 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { RouterModule } from '@angular/router';
+import { IMapMarker, PicsaMapComponent } from '@picsa/shared/features/map/map';
 
 import { ClimateDataDashboardService, IStationRow } from '../../climate-data.service';
 
 @Component({
   selector: 'dashboard-climate-data-home',
   standalone: true,
-  imports: [CommonModule, MatTableModule, RouterModule],
+  imports: [CommonModule, MatTableModule, RouterModule, PicsaMapComponent],
   templateUrl: './climate-data-home.component.html',
   styleUrls: ['./climate-data-home.component.scss'],
 })
 export class ClimateDataHomeComponent implements OnInit {
   public displayedColumns: (keyof IStationRow)[] = ['station_id', 'station_name'];
 
+  public mapMarkers: IMapMarker[];
+
   constructor(public service: ClimateDataDashboardService) {}
+
   async ngOnInit() {
     await this.service.ready();
+    this.mapMarkers = this.service.stations.map((m) => ({
+      latlng: [m.latitude as number, m.longitude as number],
+      number: m.station_id,
+    }));
   }
 }
--- libs/shared/src/features/map/map.ts
+++ libs/shared/src/features/map/map.ts
@@ -21,7 +21,17 @@ export class PicsaMapComponent {
 
   @Input() mapOptions: L.MapOptions = {};
   @Input() basemapOptions: Partial<IBasemapOptions> = {};
-  @Input() markers: IMapMarker[];
+
+  private _markers: IMapMarker[];
+  @Input() set markers(markers: IMapMarker[]) {
+    if (markers) {
+      this._markers = markers;
+      // add markers if map already initialised, otherwise will be added onMapReady
+      if (this.map) {
+        this.addMarkers(markers);
+      }
+    }
+  }
 
   // make native map element available directly
   public map: L.Map;
@@ -31,6 +41,7 @@ export class PicsaMapComponent {
   private _activeMarker: L.Marker;
   // default options are overwritten via input setter
   _mapOptions: L.MapOptions = MAP_DEFAULTS;
+
   ngOnInit() {
     // the user provides basemap options separate to general map options, so combine here
     // define the basemap layer and then bind to the view component
@@ -40,43 +51,44 @@ export class PicsaMapComponent {
     this._mapOptions = { ...mapOptions, layers: [basemap] };
   }
 
-  private addMarkers(mapMarkers: IMapMarker[], popupContent?: HTMLDivElement) {
+  private addMarkers(mapMarkers: IMapMarker[], fitMap = true) {
     mapMarkers.forEach((m, i) => {
       const icon = L.icon({
         ...ICON_DEFAULTS,
-        iconUrl: m.iconUrl,
+        iconUrl: m.iconUrl || STATION_ICON_WHITE,
       });
       const activeIcon = L.icon({
         ...ACTIVE_ICON_DEFAULTS,
-        iconUrl: m.iconUrl,
+        iconUrl: m.iconUrl || STATION_ICON_WHITE,
       });
       const marker = L.marker(m.latlng, { icon });
-      if (m.numbered) {
+      if (m.number) {
         const toolTip = L.tooltip(NUMBER_TOOLTIP_DEFAULTS);
-        marker
-          .bindTooltip(toolTip)
-          .setTooltipContent(`${i + 1}`)
-          .openTooltip();
+        marker.bindTooltip(toolTip).setTooltipContent(`${m.number}`).openTooltip();
       }
       marker.on({
         click: () => this._onMarkerClick(m, marker, activeIcon, icon),
       });
       marker.addTo(this.map);
     });
+    if (fitMap) {
+      this.fitMapToMarkers(mapMarkers);
+    }
   }
 
   // when the map is ready it emits event with map, and also binds map to
   // public api to be accessed by other services
   _onMapReady(map: L.Map) {
     this.map = map;
-    this.addMarkers(this.markers);
-    this.fitMapToMarkers();
+    if (this._markers) {
+      this.addMarkers(this._markers);
+    }
     this.onMapReady.emit(map);
   }
 
   /** Calculate a bounding rectangle that covers all points and fit within map */
-  private fitMapToMarkers() {
-    const latLngs = this.markers.map((m) => m.latlng);
+  private fitMapToMarkers(markers: IMapMarker[]) {
+    const latLngs = markers.map((m) => m.latlng);
     const bounds = new L.LatLngBounds(latLngs as any);
     this.map.fitBounds(bounds, { maxZoom: 8, padding: [10, 10] });
   }
@@ -175,12 +187,19 @@ const NUMBER_TOOLTIP_DEFAULTS: L.TooltipOptions = {
 
 type IFeaturedCountry = 'malawi' | 'kenya';
 export interface IMapMarker {
-  iconUrl: string;
+  iconUrl?: string;
   latlng: L.LatLngTuple;
-  numbered?: boolean;
+  /** Display number with icon */
+  number?: number;
   data?: any;
 }
 export interface IBasemapOptions extends L.TileLayerOptions {
   src: string;
 }
 export type IMapOptions = L.MapOptions;
+
+// svg icon hardcoded to data uri
+const STATION_ICON_BLACK =
+  "data:image/svg+xml,%3Csvg width='100px' height='100px' enable-background='new 6.191 0 87.619 100' fill='%23000000' version='1.1' viewBox='6.191 0 87.619 100' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='m82.447 57.766c-3.112 0-5.937 1.255-7.992 3.29l-19.102-13.687v-1.773c0-2.015-1.303-3.692-3.094-4.333v-18.671c5.521-0.768 9.769-5.509 9.769-11.236-1e-3 -6.266-5.082-11.356-11.361-11.356-6.266 0-11.348 5.091-11.348 11.355 0 5.727 4.247 10.468 9.77 11.236v18.688c-1.76 0.662-3.027 2.335-3.027 4.316v1.604l-20.156 13.865c-2.071-2.262-5.049-3.678-8.355-3.678-6.277 0-11.36 5.087-11.36 11.357s5.083 11.356 11.36 11.356c6.265 0 11.348-5.087 11.348-11.356 0-1.818-0.436-3.544-1.193-5.066l18.353-12.622v44.299c0 2.561 2.089 4.646 4.647 4.646 2.564 0 4.646-2.085 4.646-4.646v-44.084l17.183 12.314c-0.91 1.632-1.437 3.524-1.437 5.529 0 6.283 5.087 11.357 11.351 11.357 6.273 0 11.36-5.074 11.36-11.357-1e-3 -6.269-5.088-11.347-11.362-11.347z'/%3E%3C/svg%3E%0A";
+const STATION_ICON_WHITE =
+  "data:image/svg+xml,%3Csvg width='100px' height='100px' enable-background='new 6.191 0 87.619 100' fill='%23000000' version='1.1' viewBox='6.191 0 87.619 100' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='m82.447 57.766c-3.112 0-5.937 1.255-7.992 3.29l-19.102-13.687v-1.773c0-2.015-1.303-3.692-3.094-4.333v-18.671c5.521-0.768 9.769-5.509 9.769-11.236-1e-3 -6.266-5.082-11.356-11.361-11.356-6.266 0-11.348 5.091-11.348 11.355 0 5.727 4.247 10.468 9.77 11.236v18.688c-1.76 0.662-3.027 2.335-3.027 4.316v1.604l-20.156 13.865c-2.071-2.262-5.049-3.678-8.355-3.678-6.277 0-11.36 5.087-11.36 11.357s5.083 11.356 11.36 11.356c6.265 0 11.348-5.087 11.348-11.356 0-1.818-0.436-3.544-1.193-5.066l18.353-12.622v44.299c0 2.561 2.089 4.646 4.647 4.646 2.564 0 4.646-2.085 4.646-4.646v-44.084l17.183 12.314c-0.91 1.632-1.437 3.524-1.437 5.529 0 6.283 5.087 11.357 11.351 11.357 6.273 0 11.36-5.074 11.36-11.357-1e-3 -6.269-5.088-11.347-11.362-11.347z' fill='%23fff'/%3E%3C/svg%3E%0A";
