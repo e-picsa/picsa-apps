520a3252b8fd8efa5eedd134b1a49a8cf06ed797
FaithDaka <faithbusigu8@gmail.com>
2024-01-18 17:05:43 +0300

    feat: v1 crop information dashboard module

--- apps/picsa-apps/dashboard/src/app/pages/crop-information/crop-information.module.ts
+++ apps/picsa-apps/dashboard/src/app/pages/crop-information/crop-information.module.ts
@@ -1,12 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-
+import { CropInformationPageComponent } from './crop-information.page';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: CropInformationPageComponent,
+      },
+    ]),
+  ],
 })
-export class CropInformationModule { }
+export class CropInformationModule {}
---
+++ apps/picsa-apps/dashboard/src/app/pages/crop-information/crop-information.page.ts
@@ -0,0 +1,30 @@
+import '@uppy/core/dist/style.min.css';
+import '@uppy/dashboard/dist/style.min.css';
+
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { SupabaseService } from '@picsa/shared/services/core/supabase';
+import { UppyAngularDashboardModule } from '@uppy/angular';
+import Uppy from '@uppy/core';
+
+import { DashboardMaterialModule } from '../../material.module';
+
+@Component({
+  selector: 'dashboard-resources-page',
+  standalone: true,
+  imports: [CommonModule, DashboardMaterialModule, UppyAngularDashboardModule],
+  templateUrl: './crop-information.page.html',
+  styleUrls: ['./crop-information.page.scss'],
+})
+export class CropInformationPageComponent implements OnInit {
+  uppy: Uppy = new Uppy({ debug: true, autoProceed: true });
+
+  constructor(public supabaseService: SupabaseService) {}
+
+  async ngOnInit() {
+    const table = this.supabaseService.db.table('crop-information');
+
+    const { data, error } = await table.select();
+    console.log({ data, error });
+  }
+}
