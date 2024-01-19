0f6a5d5c5b03ff7837e9137a6f39e6614b0147fe
Ochieng Paul <ochiengpaul442@gmail.com>
2024-01-19 16:03:56 +0300

    worked on PR feedback

--- apps/picsa-tools/budget-tool/src/app/components/budget-summary/budget-summary.component.ts
+++ apps/picsa-tools/budget-tool/src/app/components/budget-summary/budget-summary.component.ts
@@ -7,20 +7,25 @@ import { Component, Input, OnInit } from '@angular/core';
       <h4>Details</h4>
       <div class="summary-list">
         <p matTooltip="Total Family Labour Hours">
-          <mat-icon>groups</mat-icon>: <b>{{ totalFamilyLabourHours }}</b>
+          <img [src]="getImagePath('family', 'svg')" alt="Groups Icon" class="icon" />:
+          <b>{{ totalFamilyLabourHours }}</b>
         </p>
         <p matTooltip="Total Inputs Value">
-          <mat-icon>receipt</mat-icon> : <b>{{ totalInputsValue }}</b>
+          <img [src]="getImagePath('inputs', 'svg')" alt="Receipt Icon" class="icon" /> : <b>{{ totalInputsValue }}</b>
         </p>
         <p matTooltip="Total Outputs Value">
-          <mat-icon>monetization_on</mat-icon>: <b>{{ totalOutputsValue }}</b>
+          <img [src]="getImagePath('outputs', 'svg')" alt="Monetization Icon" class="icon" />:
+          <b>{{ totalOutputsValue }}</b>
         </p>
-        <p matTooltip="Total Produce Consumed">
-          <mat-icon>restaurant_menu</mat-icon>
-          : <b>{{ totalProduceConsumed }}</b>
-        </p>
+        <div *ngFor="let produce of totalProduceSummary">
+          <p [matTooltip]="'Total ' + produce.label + ' consumed'">
+            <img [src]="getImagePath(produce.id, produce.extension)" [alt]="produce.label + ' Icon'" class="icon" />
+            : <b>{{ produce.total }}</b>
+          </p>
+        </div>
         <p matTooltip="Final Cash Balance">
-          <mat-icon>account_balance_wallet</mat-icon>: <b>{{ finalCashBalance }}</b>
+          <img [src]="getImagePath('cash-balance', 'svg')" alt="Wallet Icon" class="icon" />:
+          <b>{{ finalCashBalance }}</b>
         </p>
       </div>
     </div>
@@ -33,7 +38,7 @@ export class BudgetSummaryComponent implements OnInit {
   totalFamilyLabourHours: number = 0;
   totalInputsValue: number = 0;
   totalOutputsValue: number = 0;
-  totalProduceConsumed: number = 0;
+  totalProduceSummary: any[] = [];
   finalCashBalance: number = 0;
 
   ngOnInit(): void {
@@ -55,10 +60,29 @@ export class BudgetSummaryComponent implements OnInit {
       });
 
       item.produceConsumed.forEach((consumed) => {
-        this.totalProduceConsumed += consumed.values.quantity;
+        this.updateProduceSummary(consumed);
       });
     });
 
     this.finalCashBalance = this.totalOutputsValue - this.totalInputsValue;
+  }
+
+  updateProduceSummary(consumed: any) {
+    const existingProduce = this.totalProduceSummary.find((p) => p.label === consumed.label);
+
+    if (existingProduce) {
+      existingProduce.total += consumed.values.quantity;
+    } else {
+      this.totalProduceSummary.push({
+        label: consumed.label,
+        total: consumed.values.quantity,
+        id: consumed.id,
+        extension: consumed.imgType,
+      });
+    }
+  }
+
+  getImagePath(imageId: string, extension: 'png' | 'svg'): string {
+    return `assets/budget-cards/${imageId}.${extension}`;
   }
 }
