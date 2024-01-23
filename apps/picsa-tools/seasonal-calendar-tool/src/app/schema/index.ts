52bdf8b8c2369f26ba47b94f6b6a2dc2baf94b9f
chrismclarke <chris.m.clarke@live.co.uk>
2024-01-22 21:01:24 -0800

    refactor: calendar schema

--- apps/picsa-tools/seasonal-calendar-tool/src/app/schema/index.ts
+++ apps/picsa-tools/seasonal-calendar-tool/src/app/schema/index.ts
@@ -1,10 +1,7 @@
 // import * as schema from './schema_v0';
-import * as schema_v1 from './schema_v1';
+// import * as schema_v1 from './schema_v1';
+import * as schema from './schema_v2';
 
-export const COLLECTION = schema_v1.COLLECTION_V1;
-export type CalendarDataEntry = schema_v1.CalendarDataEntry_v1;
-export const SCHEMA = schema_v1.SCHEMA_V1;
-
-// Utility types created from main entry
-export type CropEntry = CalendarDataEntry['crops'][number];
-export type CalendarPeriod = CalendarDataEntry['timeAndConditions'][number];
+export const COLLECTION = schema.COLLECTION_V2;
+export type CalendarDataEntry = schema.CalendarDataEntry_v2;
+export const SCHEMA = schema.SCHEMA_V2;
--- apps/picsa-tools/seasonal-calendar-tool/src/app/schema/schema_v2.ts
+++ apps/picsa-tools/seasonal-calendar-tool/src/app/schema/schema_v2.ts
@@ -1,33 +1,63 @@
+import { MONTH_NAMES } from '@picsa/data';
 import type { IPicsaCollectionCreator } from '@picsa/shared/services/core/db_v2';
 import { RxJsonSchema } from 'rxdb';
 
 import { CalendarDataEntry_v1, COLLECTION_V1, SCHEMA_V1 } from './schema_v1';
 
-export interface CalendarDataEntry_v2 extends Omit<CalendarDataEntry_v1, 'crops'> {
-  /** names of crops with list of activities to match months */
-  crops: { name: string; activities: string[] }[];
+export const CALENDAR_ENTRY_MOCK_v2: CalendarDataEntry_v2 = {
+  ID: 'mock_01',
+  name: 'Mock Entry',
+  activities: {
+    cassava: ['planting', 'sowing'],
+  },
+  weather: ['sunny', 'cloudy'],
+  meta: {
+    startMonth: 4,
+    timePeriods: 2,
+    crops: ['cassava'],
+  },
+};
+
+// New interface from scratch (ignore v1)
+export interface CalendarDataEntry_v2 {
+  ID: string;
+  name: string;
+  activities: {
+    [enterprise: string]: string[];
+  };
+  weather: string[];
+  meta: {
+    startMonth: number;
+    timePeriods: number;
+    crops: string[];
+  };
 }
 
 export const SCHEMA_V2: RxJsonSchema<CalendarDataEntry_v2> = {
   ...SCHEMA_V1,
   version: 2,
   properties: {
-    ...SCHEMA_V1.properties,
-    crops: {
+    ID: {
+      type: 'string',
+    },
+    name: {
+      type: 'string',
+      default: '',
+    },
+    activities: {
+      type: 'object',
+      additionalProperties: { type: 'array', items: { type: 'string' } },
+    },
+    weather: {
       type: 'array',
-      items: {
-        properties: {
-          name: {
-            type: 'string',
-          },
-          activities: {
-            type: 'array',
-            items: {
-              type: 'string',
-            },
-          },
-        },
-        required: ['name'],
+      items: { type: 'string' },
+    },
+    meta: {
+      type: 'object',
+      properties: {
+        startMonth: { type: 'integer' },
+        timePeriods: { type: 'integer' },
+        crops: { type: 'array', items: { type: 'string' } },
       },
     },
   },
@@ -43,14 +73,22 @@ export const COLLECTION_V2: IPicsaCollectionCreator<CalendarDataEntry_v2> = {
   migrationStrategies: {
     ...COLLECTION_V1.migrationStrategies,
     2: (data: CalendarDataEntry_v1): CalendarDataEntry_v2 => {
-      const { crops, ...existing } = data;
+      const { crops, ID, name, timeAndConditions } = data;
+      const startMonth = MONTH_NAMES.findIndex(({ id }) => id === timeAndConditions[0]?.month) || 0;
+      const activities: Record<string, string[]> = {};
+      for (const crop of crops) {
+        activities[crop.name] = crop.months.map((c) => c.activities[0] || '');
+      }
       return {
-        ...existing,
-        // update crops to only include name and remove previous meta
-        crops: crops.map((c) => ({
-          name: c.name,
-          activities: (c.months || []).map((c) => c.activities[0] || ''),
-        })),
+        ID,
+        name,
+        activities,
+        meta: {
+          startMonth,
+          timePeriods: timeAndConditions.length,
+          crops: crops.map((c) => c.name),
+        },
+        weather: timeAndConditions.map(({ weather }) => weather),
       };
     },
   },
