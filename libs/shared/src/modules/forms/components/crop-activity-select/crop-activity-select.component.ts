f8498d77a665e44b3f8ba2250f1bc48e8f732424
chrismclarke <chris.m.clarke@live.co.uk>
2024-01-27 19:33:52 -0800

    refactor: crop activity and weather select components

--- libs/shared/src/modules/forms/components/crop-activity-select/crop-activity-select.component.ts
+++ libs/shared/src/modules/forms/components/crop-activity-select/crop-activity-select.component.ts
@@ -1,16 +1,9 @@
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  EventEmitter,
-  forwardRef,
-  Input,
-  Output,
-  Provider,
-} from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, forwardRef, Provider } from '@angular/core';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
-import { CROP_ACTIVITY_DATA, CROP_ACTIVITY_HASHMAP } from '@picsa/data';
+import { CROP_ACTIVITY_DATA, CROP_ACTIVITY_HASHMAP, ICropActivityDataEntry } from '@picsa/data';
+
+import { BaseSelectComponent } from '../base/select';
 
 /** Accessor used for binding with ngModel or formgroups */
 export const CONTROL_VALUE_ACCESSOR: Provider = {
@@ -30,53 +23,11 @@ export const CONTROL_VALUE_ACCESSOR: Provider = {
   providers: [CONTROL_VALUE_ACCESSOR],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FormCropActivitySelectComponent {
-  protected selectOptions = CROP_ACTIVITY_DATA;
+export class FormCropActivitySelectComponent extends BaseSelectComponent<ICropActivityDataEntry> {
+  public override selectOptions = CROP_ACTIVITY_DATA;
+  public override selectOptionsHashmap = CROP_ACTIVITY_HASHMAP;
 
-  /** Selected value binding */
-  @Input()
-  get selected() {
-    return this._selected;
-  }
-  set selected(selected: string) {
-    selected ??= '';
-    if (this.selected !== selected) {
-      this._selected = selected;
-      this.cdr.markForCheck();
-      this.selectedChange.emit(this._selected);
-      if (this._onChange) {
-        this._onChange(this._selected);
-      }
-    }
-  }
-
-  /** Get full selected entry data */
-  protected get selectedData() {
-    return CROP_ACTIVITY_HASHMAP[this.selected];
-  }
-
-  /** Additional event emitter to allow manual bind to <gender-input (selectedChange) /> event*/
-  @Output() selectedChange = new EventEmitter<string>();
-
-  private _selected = ''; // this is the updated value that the class accesses
-
-  constructor(public dialog: MatDialog, private cdr: ChangeDetectorRef) {}
-
-  /** Events registered by ngModel and Form Controls */
-  // eslint-disable-next-line @typescript-eslint/member-ordering
-  private _onChange: (value: string) => void;
-  // eslint-disable-next-line @typescript-eslint/member-ordering
-  private _onTouched: (value: string) => void;
-
-  writeValue(selected: string) {
-    this.selected = selected;
-  }
-
-  registerOnChange(fn: (value: string) => void) {
-    this._onChange = fn;
-  }
-
-  registerOnTouched(fn: (value: string) => void) {
-    this._onTouched = fn; // <-- save the function
+  constructor(cdr: ChangeDetectorRef, public dialog: MatDialog) {
+    super(cdr);
   }
 }
--- libs/shared/src/modules/forms/components/weather-select/weather-select.component.ts
+++ libs/shared/src/modules/forms/components/weather-select/weather-select.component.ts
@@ -1,16 +1,9 @@
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  EventEmitter,
-  forwardRef,
-  Input,
-  Output,
-  Provider,
-} from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, forwardRef, Provider } from '@angular/core';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
-import { WEATHER_DATA, WEATHER_DATA_HASHMAP } from '@picsa/data/weather';
+import { IWeatherDataEntry, WEATHER_DATA, WEATHER_DATA_HASHMAP } from '@picsa/data/weather';
+
+import { BaseSelectComponent } from '../base/select';
 
 /** Accessor used for binding with ngModel or formgroups */
 export const CONTROL_VALUE_ACCESSOR: Provider = {
@@ -30,52 +23,11 @@ export const CONTROL_VALUE_ACCESSOR: Provider = {
   providers: [CONTROL_VALUE_ACCESSOR],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FormWeatherSelectComponent {
-  protected selectOptions = WEATHER_DATA.filter((w) => w.label !== '');
+export class FormWeatherSelectComponent extends BaseSelectComponent<IWeatherDataEntry> {
+  public override selectOptions = WEATHER_DATA.filter((w) => w.label !== '');
+  public override selectOptionsHashmap = WEATHER_DATA_HASHMAP;
 
-  /** Selected value binding */
-  @Input()
-  get selected() {
-    return this._selected;
-  }
-  set selected(selected: string) {
-    selected ??= '';
-    if (this.selected !== selected) {
-      this._selected = selected;
-      this.cdr.markForCheck();
-      this.selectedChange.emit(this._selected);
-      if (this._onChange) {
-        this._onChange(this._selected);
-      }
-    }
-  }
-  /** Get full selected entry data */
-  protected get selectedData() {
-    return WEATHER_DATA_HASHMAP[this.selected];
-  }
-
-  /** Additional event emitter to allow manual bind to <gender-input (selectedChange) /> event*/
-  @Output() selectedChange = new EventEmitter<string>();
-
-  private _selected = ''; // this is the updated value that the class accesses
-
-  constructor(public dialog: MatDialog, private cdr: ChangeDetectorRef) {}
-
-  /** Events registered by ngModel and Form Controls */
-  // eslint-disable-next-line @typescript-eslint/member-ordering
-  private _onChange: (value: string) => void;
-  // eslint-disable-next-line @typescript-eslint/member-ordering
-  private _onTouched: (value: string) => void;
-
-  writeValue(selected: string) {
-    this.selected = selected;
-  }
-
-  registerOnChange(fn: (value: string) => void) {
-    this._onChange = fn;
-  }
-
-  registerOnTouched(fn: (value: string) => void) {
-    this._onTouched = fn; // <-- save the function
+  constructor(cdr: ChangeDetectorRef, public dialog: MatDialog) {
+    super(cdr);
   }
 }
