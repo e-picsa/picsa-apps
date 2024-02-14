a88cab6c4fb4b4f164c586b4381ebb19b6ac170f
chrismclarke <chris.m.clarke@live.co.uk>
2024-02-13 22:09:07 -0800

    refactor: climate api service client

--- apps/picsa-apps/dashboard/src/app/modules/climate/climate-api.service.ts
+++ apps/picsa-apps/dashboard/src/app/modules/climate/climate-api.service.ts
@@ -1,32 +1,27 @@
 import { Injectable } from '@angular/core';
-import { PicsaNotificationService } from '@picsa/shared/services/core/notification.service';
 import createClient from 'openapi-fetch';
+import {  BehaviorSubject, Subject } from 'rxjs';
 
 import { paths } from './types/api';
 
 const API_ENDPOINT = 'https://api.epicsa.idems.international';
 
-/** Custom client which tracks responses by callback id */
-type ICallbackClient = (id:string)=>ReturnType<typeof createClient<paths>>
-
 /** Type-safe http client with added support for callbacks */
-type IClient = ReturnType<typeof createClient<paths>> & {useMeta:ICallbackClient}
-
-interface IMetaEntry{
-  status:'pending' | 'success' | 'error' | 'unknown',
-  rawResponse?:Response,
-}
-
+export type IApiClient = ReturnType<typeof createClient<paths>> & { $:Subject<Response | undefined>}
 
 /** 
  * Service to interact with external PICSA Climate API 
  * All methods are exposed through a type-safe `client` property, or can additionally use
  * a custom client that includes status notification updates via the `useMeta` method
  * @example
- * Use custom callback that will show user notifications on error and record to service
+ * Use observable client that can also be accessed by api-status component
  * ```ts
- * const {response, data, error} = await api.useMeta('myRequestId').POST(...)
+ * await api.getObservableClient('myRequestId').POST(...)
  * ```
+ * ```html
+ * <dashboard-climate-api-status clientId='myRequestId' />
+ * ```
+ * 
  * Use default client without additional callbacks
  * ```ts
  * const {response, data, error} = await api.client.POST(...)
@@ -34,64 +29,45 @@ interface IMetaEntry{
  * */
 @Injectable({ providedIn: 'root' })
 export class ClimateApiService {
-
-  /** Request additional meta by id */
-  public meta:Record<string ,IMetaEntry>={}
   
   /** Http client with type-definitions for API endpoints */
-  public client:IClient
+  public client:IApiClient
 
-  constructor(private notificationService:PicsaNotificationService) {
-    const client = createClient<paths>({ baseUrl: API_ENDPOINT,mode:'cors' });
-    this.client = {...client,useMeta:()=>{
-      return client
-    }}
+  /** List of custom clients generated to be shared across observers */
+  private observableClients:Record<string,IApiClient> = {}
+
+  constructor() {
+    this.client = this.createObservableClient('_default')
   }
 
-
-  /** 
-   * Provide an id which which will be updated alongside requests. 
-   * The cache will also include interceptors to provide user notification on error 
-   **/
-  public useMeta(id:string){
-    const customFetch = this.createCustomFetchClient(id)
-    const customClient = createClient<paths>({ baseUrl: API_ENDPOINT,mode:'cors',fetch:customFetch });
-    return customClient
+  /**
+   * Retrive an instance of the api client with a specific ID so that request updates can be subscribed to
+   * by any other services or components accessing via the same id. Creates new client if not existing
+   */
+  public getObservableClient(clientId?:string):IApiClient{
+    if(clientId){
+      return this.observableClients[clientId] || this.createObservableClient(clientId)
+    }
+    return this.client
   }
 
-  /** Create a custom implementation of fetch client to handle status updates and notifications */
-  private createCustomFetchClient(id:string){
-   return async (...args:Parameters<typeof window['fetch']>)=>{
-      this.meta[id]={status:'pending'}
+  private createObservableClient(clientId:string){
+    const $ = new BehaviorSubject<Response | undefined>(undefined)
+    const customFetch = async (...args:Parameters<typeof window['fetch']>)=>{
+      // send a custom response with 102 status code to inform that request has been sent but is pending
+      $.next({status:102} as Response)
       const response = await window.fetch(...args);
-      this.meta[id].status =  this.getCallbackStatus(response.status)
-      this.meta[id].rawResponse = response
-      if(this.meta[id].status ==='error' ){
-        await this.showCustomFetchErrorMessage(id,response)
-      }
+      $.next(response)      
       return response
     }
+    const baseClient = createClient<paths>({ baseUrl: API_ENDPOINT,mode:'cors',fetch:customFetch });
+    const client:IApiClient ={...baseClient, $}
+    this.observableClients[clientId] = client
+    return client
   }
 
-  /** Show error message when using custom fetch with callbacks */
-  private async showCustomFetchErrorMessage(id:string,response:Response){
-    // clone body so that open-api can still consume when constructing full fetch response
-    const clone = response.clone()
-    try {
-      const json  = await clone.json()
-      const errorText = json.detail || 'failed, see console logs for details'
-      this.notificationService.showUserNotification({matIcon:'error',message:`[${id}] ${errorText}`})
-    } catch (error) {
-      console.error(error)
-      console.error('Fetch Error',error)
-      this.notificationService.showUserNotification({matIcon:'error',message:`[${id}] 'failed, see console logs for details'`})
-    }
-  }
 
-  private getCallbackStatus(statusCode:number):IMetaEntry['status']{
-    if(200 <= statusCode && statusCode <=299) return 'success'
-    if(400 <= statusCode && statusCode <=499) return 'error'
-    if(500 <= statusCode && statusCode <=599) return 'error'
-    return 'unknown'
-  }
+
+
+ 
 }
