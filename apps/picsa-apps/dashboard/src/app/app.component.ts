e917ae0dd4e5b023b71ea32dab77e3052350b74f
chrismclarke <chris.m.clarke@live.co.uk>
2024-02-27 11:37:02 -0800

    feat: add support for nav icons

--- apps/picsa-apps/dashboard/src/app/app.component.ts
+++ apps/picsa-apps/dashboard/src/app/app.component.ts
@@ -3,13 +3,8 @@ import { AfterViewInit, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SupabaseService } from '@picsa/shared/services/core/supabase';
 
+import { DASHBOARD_NAV_LINKS, INavLink } from './data';
 import { DashboardMaterialModule } from './material.module';
-
-interface INavLink {
-  label: string;
-  href: string;
-  children?: INavLink[];
-}
 
 @Component({
   standalone: true,
@@ -21,38 +16,7 @@ interface INavLink {
 export class AppComponent implements AfterViewInit {
   title = 'picsa-apps-dashboard';
 
-  navLinks: INavLink[] = [
-    {
-      label: 'Resources',
-      href: '/resources',
-    },
-    {
-      label: 'Climate',
-      href: '/climate',
-      children: [
-        {
-          label: 'Station Data',
-          href: '/station',
-        },
-        {
-          label: 'Forecasts',
-          href: '/forecast',
-        },
-      ],
-    },
-    // {
-    //   label: 'Crop Information',
-    //   href: '/crop-information',
-    // },
-    // {
-    //   label: 'Monitoring Forms',
-    //   href: '/monitoring-forms',
-    // },
-    {
-      label: 'Translations',
-      href: '/translations',
-    },
-  ];
+  navLinks = DASHBOARD_NAV_LINKS;
 
   globalLinks: INavLink[] = [
     // {
