c7bc555ab3dfac94f74516be20049b289a09b885
chrismclarke <chris.m.clarke@live.co.uk>
2024-02-28 11:19:14 -0800

    feat: unique storage path db

--- apps/picsa-server/supabase/migrations/20240228005632_deployments_table_add.sql
+++ apps/picsa-server/supabase/migrations/20240228005632_deployments_table_add.sql
@@ -7,9 +7,11 @@ create table
     variant text null default 'extension'::text,
     access_key_md5 text null,
     public boolean not null default true,
+    icon_path text null,
     constraint deployments_pkey primary key (id),
+    constraint deployments_icon_path_fkey foreign key (icon_path) references storage.objects (path),
     constraint deployments_variant_check check (
-       (
+      (
         (variant = 'farmer'::text)
         or (variant = 'extension'::text)
         or (variant = 'other'::text)
