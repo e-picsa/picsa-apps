8fc4c332d4a3f0297c1905b40e13f89929154bc1
chrismclarke <chris.m.clarke@live.co.uk>
2024-02-28 14:32:20 -0800

    feat: deployment icon and storage path pipe

--- apps/picsa-apps/dashboard/src/app/modules/deployment/pages/list/deployment-list.component.ts
+++ apps/picsa-apps/dashboard/src/app/modules/deployment/pages/list/deployment-list.component.ts
@@ -3,16 +3,25 @@ import { Component, effect, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { formatHeaderDefault, IDataTableOptions, PicsaDataTableComponent } from '@picsa/shared/features';
+import { StoragePathPipe } from '@picsa/shared/services/core/supabase';
 
+import { DashboardStorageLinkComponent } from '../../../../components/storage-link/storage-link.component';
 import { DeploymentDashboardService } from '../../deployment.service';
 import { IDeploymentRow } from '../../types';
 
-const DISPLAYED_COLUMNS: (keyof IDeploymentRow)[] = ['country_code', 'label', 'public', 'access_key_md5'];
+const DISPLAYED_COLUMNS: (keyof IDeploymentRow)[] = ['country_code', 'label', 'public', 'access_key_md5', 'icon_path'];
 
 @Component({
   selector: 'dashboard-deployment-list',
   standalone: true,
-  imports: [CommonModule, MatIconModule, MatTabsModule, PicsaDataTableComponent],
+  imports: [
+    CommonModule,
+    DashboardStorageLinkComponent,
+    MatIconModule,
+    MatTabsModule,
+    PicsaDataTableComponent,
+    StoragePathPipe,
+  ],
   templateUrl: './deployment-list.component.html',
   styleUrls: ['./deployment-list.component.scss'],
 })
@@ -26,6 +35,7 @@ export class DeploymentListComponent implements OnInit {
     displayColumns: DISPLAYED_COLUMNS,
     formatHeader: (value) => {
       if (value === 'access_key_md5') return 'Restricted';
+      if (value === 'icon_path') return 'Icon';
       return formatHeaderDefault(value);
     },
   };
--- apps/picsa-server/supabase/migrations/20240228005632_deployments_table_add.sql
+++ apps/picsa-server/supabase/migrations/20240228005632_deployments_table_add.sql
@@ -9,7 +9,7 @@ create table
     public boolean not null default true,
     icon_path text null,
     constraint deployments_pkey primary key (id),
-    constraint deployments_icon_path_fkey foreign key (icon_path) references storage.objects (path),
+    constraint deployments_icon_path_fkey foreign key (icon_path) references storage.objects (path) on delete set null,
     constraint deployments_variant_check check (
       (
         (variant = 'farmer'::text)
