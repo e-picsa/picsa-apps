a45618d993292a17b9f5b20a5d3c65a62cfd6aa0
FaithDaka <faithbusigu8@gmail.com>
2024-03-10 11:43:52 +0300

    fix: updated table functions

--- apps/picsa-apps/dashboard/src/app/modules/crop-information/crop-information.page.ts
+++ apps/picsa-apps/dashboard/src/app/modules/crop-information/crop-information.page.ts
@@ -3,42 +3,52 @@ import '@uppy/dashboard/dist/style.min.css';
 
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { PicsaNotificationService } from '../../../../../../../libs/shared/src/services/core/notification.service';
 
 import { DashboardMaterialModule } from '../../material.module';
 import { CropProbabilityDashboardService } from './crop-information.service';
+import { PicsaDataTableComponent } from '../../../../../../../libs/shared/src/features';
 
 @Component({
   selector: 'dashboard-resources-page',
   standalone: true,
-  imports: [CommonModule, DashboardMaterialModule],
+  imports: [CommonModule, DashboardMaterialModule, PicsaDataTableComponent, RouterModule],
   templateUrl: '../crop-information/crop-information.component.html',
   styleUrls: ['../crop-information/crop-information.component.scss'],
 })
 export class CropInformationPageComponent implements OnInit {
-  constructor(public service: CropProbabilityDashboardService, private router: Router) {}
+  constructor(
+    public service: CropProbabilityDashboardService,
+    private notificationService: PicsaNotificationService,
+  ) {}
 
   displayedColumns: string[] = [
-    'station_name',
+    'crop',
+    'variety',
     'water_lower',
     'water_upper',
     'length_lower',
     'length_upper',
-    'station_notes',
+    'label'
   ];
 
-  ngOnInit(): void {
+  tableOptions = {
+    displayColumns: this.displayedColumns,
+    handleRowClick: () => null,
+  };
+
+  async ngOnInit() {
     this.service.ready();
     this.refreshCropInformation();
   }
 
   refreshCropInformation() {
     this.service.listCropProbabilities().catch((error) => {
-      console.error('Error fetching crop probabilities:', error);
+      this.notificationService.showUserNotification({
+        matIcon: 'error',
+        message: 'Error fetching crop probabilities:' + error.message,
+      });
     });
-  }
-
-  navigateToEntryPage() {
-    this.router.navigate(['/crop-information/entry']);
   }
 }
