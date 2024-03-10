4ae3ac8e1647ae3a3d704990a8656fc883e3c898
FaithDaka <faithbusigu8@gmail.com>
2024-03-10 11:45:12 +0300

    refactor: entry component uses form builder

--- apps/picsa-apps/dashboard/src/app/modules/crop-information/pages/new_entry/new_entry.page.ts
+++ apps/picsa-apps/dashboard/src/app/modules/crop-information/pages/new_entry/new_entry.page.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { Router } from '@angular/router';
 
 import { DashboardMaterialModule } from '../../../../material.module';
 import { CropProbabilityDashboardService } from '../../crop-information.service';
@@ -15,41 +15,35 @@ import { CropProbabilityDashboardService } from '../../crop-information.service'
   styleUrls: ['./new_entry.component.scss'],
 })
 export class NewEntryPageComponent implements OnInit {
-  station_name: string;
-  water_lower: number;
-  water_upper: number;
-  length_lower: number;
-  length_upper: number;
-  station_notes: string[];
+  entryForm = this.formBuilder.group({
+    crop: [''],
+    variety: [''],
+    water_lower: [0],
+    water_upper: [0],
+    length_lower: [0],
+    length_upper: [0],
+  });
   ActionFeedbackMessage: string;
-  constructor(private service: CropProbabilityDashboardService, private router: Router) {
+
+  constructor(private service: CropProbabilityDashboardService, private formBuilder: FormBuilder) {
     this.service.ready();
   }
+
   ngOnInit(): void {
     this.service.ready();
   }
   submitForm() {
-    const data = {
-      station_name: this.station_name,
-      water_lower: this.water_lower,
-      water_upper: this.water_upper,
-      length_lower: this.length_lower,
-      length_upper: this.length_upper,
-      station_notes: this.station_notes,
-    };
+    const formData = this.entryForm.value;
     this.service
-      .addCropProbability(data)
+      .addCropProbability(formData)
       .then((data) => {
         if (data === 'Added successfully') {
-          this.ActionFeedbackMessage = 'Added successfully';
+          this.ActionFeedbackMessage = 'Entry added successfully';
         }
       })
       .catch((error) => {
-        console.error('Error adding translation:', error);
-        this.ActionFeedbackMessage = 'Failed to add a translation.';
+        console.error('Error adding new entry:', error);
+        this.ActionFeedbackMessage = 'Error adding new entry';
       });
-  }
-  routeBack() {
-    this.router.navigate(['/crop-information']);
   }
 }
