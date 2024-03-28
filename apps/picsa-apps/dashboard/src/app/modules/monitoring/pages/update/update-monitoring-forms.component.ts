7572c4ce5bb3acd87388eaafea8b1320678f5fdc
chrismclarke <chris.m.clarke@live.co.uk>
2024-03-27 19:33:00 -0700

    refactor: failed conversion delete and tidying

--- apps/picsa-apps/dashboard/src/app/modules/monitoring/pages/update/update-monitoring-forms.component.ts
+++ apps/picsa-apps/dashboard/src/app/modules/monitoring/pages/update/update-monitoring-forms.component.ts
@@ -9,6 +9,7 @@ import {
   SupabaseStoragePickerDirective,
   SupabaseUploadComponent,
 } from '@picsa/shared/services/core/supabase';
+import { SupabaseStorageService } from '@picsa/shared/services/core/supabase/services/supabase-storage.service';
 import { NgxJsonViewerModule } from 'ngx-json-viewer';
 
 import { DashboardMaterialModule } from '../../../../material.module';
@@ -37,7 +38,13 @@ export class UpdateMonitoringFormsComponent implements OnInit {
   public updateFeedbackMessage = '';
   public uploading = false;
   public allowedFileTypes = ['xlsx', 'xls'].map((ext) => `.${ext}`);
-  constructor(private service: MonitoringFormsDashboardService, private route: ActivatedRoute) {}
+  public storageBucketName = 'global';
+  public storageFolderPath = 'monitoring/forms';
+  constructor(
+    private service: MonitoringFormsDashboardService,
+    private route: ActivatedRoute,
+    private storageService: SupabaseStorageService
+  ) {}
   async ngOnInit() {
     await this.service.ready();
     this.route.params.subscribe(async (params) => {
@@ -53,62 +60,44 @@ export class UpdateMonitoringFormsComponent implements OnInit {
     });
   }
 
-  public async handleUploadComplete(res: IUploadResult[], controlName: 'submission_forms') {
+  public async handleUploadComplete(res: IUploadResult[]) {
     if (res.length === 0) {
       return;
     }
+    // As conversion is a 2-step process (xls file -> xml form -> enketo form) track progress
+    // so that uploaded file can be removed if not successful
+    let xformConversionSuccess = false;
     this.uploading = true;
-    const [{ entry }] = res;
-    const [{ data }] = res;
-    this.service.submitFormToConvertXlsToXForm(data).subscribe({
-      next: (response) => {
-        const xFormResponse = response['result'];
-        const blob = new Blob([xFormResponse], { type: 'text/xml' });
+    const [{ data, entry }] = res;
 
-        const xmlFile = new File([blob], 'form.xml', { type: 'text/xml' });
+    const xform = await this.service.submitFormToConvertXlsToXForm(data as File);
 
-        const formData = new FormData();
-        formData.append('files', xmlFile);
-        this.service.submitFormToConvertXFormToEnketo(formData).subscribe({
-          next: async (response) => {
-            const convertedContent = response['convertedFiles'][0]['content'];
-            //since path is not returned and it is what the relation requires
-            //SUGGESTION: we could make this reference the entry's id instead
-            if (entry) {
-              this.form.form_xlsx = `resources/${controlName}/${entry.name}`;
-            }
-            if (this.form.enketo_definition) {
-              this.form.enketo_definition['theme'] = convertedContent.theme;
-              this.form.enketo_definition['languageMap'] = convertedContent.languageMap;
-            }
+    if (xform) {
+      const blob = new Blob([xform], { type: 'text/xml' });
+      const xmlFile = new File([blob], 'form.xml', { type: 'text/xml' });
+      const formData = new FormData();
+      formData.append('files', xmlFile);
 
-            this.form.enketo_form = convertedContent.form;
-            this.form.enketo_model = convertedContent.model;
-            this.service
-              .updateFormById(this.form.id, this.form)
-              .then((data) => {
-                this.form = data;
-                this.updateFeedbackMessage = 'Form updated successfully!';
-                this.uploading = false;
-              })
-              .catch((error) => {
-                console.error('Failed to update form in database:', error);
-                this.updateFeedbackMessage = 'Failed to update form. Please try again.';
-                this.uploading = false;
-              });
-          },
-          error: (error) => {
-            console.error('Error occurred while converting to enketo:', error);
-            this.updateFeedbackMessage = 'Failed to convert form to Enketo. Please try again.';
-            this.uploading = false;
-          },
+      const enketoContent = await this.service.submitFormToConvertXFormToEnketo(formData);
+      if (enketoContent) {
+        const { form, languageMap, model, theme } = enketoContent;
+        // Update db entry with form_xlsx
+        this.form = await this.service.updateFormById(this.form.id, {
+          form_xlsx: `${this.storageBucketName}/${this.storageFolderPath}/${entry.name}`,
+          enketo_form: form,
+          enketo_model: model,
+          enketo_definition: { ...(this.form.enketo_definition as any), languageMap, theme },
         });
-      },
-      error: (error) => {
-        console.error('Error occurred while converting to xform:', error);
-        this.updateFeedbackMessage = 'Failed to convert form to XForm. Please try again.';
+        this.updateFeedbackMessage = 'Form updated successfully!';
         this.uploading = false;
-      },
-    });
+        xformConversionSuccess = true;
+      }
+    }
+    // If conversion not successful delete file from storage
+    if (!xformConversionSuccess) {
+      const storagePath = `${this.storageFolderPath}/${entry.name}`;
+      const { error } = await this.storageService.deleteFile(this.storageBucketName, storagePath);
+      if (error) throw error;
+    }
   }
 }
