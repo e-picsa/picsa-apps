45623a1ed5c813417d095659144669d8c53791c8
chrismclarke <chris.m.clarke@live.co.uk>
2024-04-01 16:48:50 -0700

    feat: add auth role bindings to home links

--- apps/picsa-apps/dashboard/src/app/app.component.ts
+++ apps/picsa-apps/dashboard/src/app/app.component.ts
@@ -3,15 +3,23 @@ import { AfterViewInit, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SupabaseService } from '@picsa/shared/services/core/supabase';
 
-import { DASHBOARD_NAV_LINKS, INavLink } from './data';
+import { DASHBOARD_NAV_LINKS, GLOBAL_NAV_LINKS } from './data';
 import { DashboardMaterialModule } from './material.module';
+import { AuthRoleRequiredDirective } from './modules/auth';
 import { DeploymentSelectComponent } from './modules/deployment/components';
 import { DeploymentDashboardService } from './modules/deployment/deployment.service';
 import { ProfileMenuComponent } from './modules/profile/components/profile-menu/profile-menu.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, DashboardMaterialModule, DeploymentSelectComponent, CommonModule, ProfileMenuComponent],
+  imports: [
+    RouterModule,
+    DashboardMaterialModule,
+    DeploymentSelectComponent,
+    CommonModule,
+    ProfileMenuComponent,
+    AuthRoleRequiredDirective,
+  ],
   selector: 'dashboard-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
@@ -19,18 +27,7 @@ import { ProfileMenuComponent } from './modules/profile/components/profile-menu/
 export class AppComponent implements AfterViewInit {
   title = 'picsa-apps-dashboard';
   navLinks = DASHBOARD_NAV_LINKS;
-
-  globalLinks: INavLink[] = [
-    {
-      label: 'Deployments',
-      href: '/deployment',
-      matIcon: 'apps',
-    },
-    // {
-    //   label: 'Users',
-    //   href: '/users',
-    // },
-  ];
+  globalLinks = GLOBAL_NAV_LINKS;
 
   constructor(public supabaseService: SupabaseService, private deploymentService: DeploymentDashboardService) {}
 
--- apps/picsa-apps/dashboard/src/app/modules/home/home.component.ts
+++ apps/picsa-apps/dashboard/src/app/modules/home/home.component.ts
@@ -4,13 +4,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 
 import { DASHBOARD_NAV_LINKS } from '../../data';
+import { AuthRoleRequiredDirective } from '../auth';
 
 @Component({
   selector: 'dashboard-home',
   templateUrl: 'home.component.html',
   styleUrl: 'home.component.scss',
   standalone: true,
-  imports: [MatCardModule, MatIconModule, RouterModule],
+  imports: [MatCardModule, MatIconModule, RouterModule, AuthRoleRequiredDirective],
 })
 export class DashboardHomeComponent {
   public links = DASHBOARD_NAV_LINKS.filter((l) => l.href !== '/home');
