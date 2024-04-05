0d4d0113449d9f054657a6c3e1a7ae71c33848ba
chrismclarke <chris.m.clarke@live.co.uk>
2024-04-05 09:18:06 -0700

    feat: wip dashboard resource type pages

--- apps/picsa-apps/dashboard/src/app/modules/resources/pages/files/resource-files.component.ts
+++ apps/picsa-apps/dashboard/src/app/modules/resources/pages/files/resource-files.component.ts
@@ -1,11 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { DashboardMaterialModule } from '../../../../material.module';
+import { DashboardResourcesStorageLinkComponent } from '../../components/storage-link/storage-link.component';
+import { ResourcesDashboardService } from '../../resources.service';
 
 @Component({
   selector: 'dashboard-resource-files',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CommonModule, DashboardMaterialModule, DashboardResourcesStorageLinkComponent, RouterModule],
   templateUrl: './resource-files.component.html',
   styleUrl: './resource-files.component.scss',
 })
-export class ResourceFilesComponent {}
+export class ResourceFilesComponent implements OnInit {
+  public displayedColumns = ['title', 'storage_file'];
+  constructor(public service: ResourcesDashboardService) {}
+  async ngOnInit() {
+    await this.service.ready();
+  }
+}
--- apps/picsa-apps/dashboard/src/app/modules/resources/resources.service.ts
+++ apps/picsa-apps/dashboard/src/app/modules/resources/resources.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, signal } from '@angular/core';
 // eslint-disable-next-line @nx/enforce-module-boundaries
-import { Database } from '@picsa/server-types';
 import { PicsaAsyncService } from '@picsa/shared/services/asyncService.service';
 import { PicsaNotificationService } from '@picsa/shared/services/core/notification.service';
 import { SupabaseService } from '@picsa/shared/services/core/supabase';
@@ -10,7 +9,7 @@ import {
 } from '@picsa/shared/services/core/supabase/services/supabase-storage.service';
 import { arrayToHashmap } from '@picsa/utils';
 
-import { IResourceRow } from './types';
+import { IResourceCollectionRow, IResourceFileChildRow, IResourceFileRow, IResourceLinkRow } from './types';
 
 export interface IResourceStorageEntry extends IStorageEntry {
   /** Url generated when upload to public bucket (will always be populated, even if bucket not public) */
@@ -21,10 +20,20 @@ export interface IResourceStorageEntry extends IStorageEntry {
 export class ResourcesDashboardService extends PicsaAsyncService {
   private storageFiles: IResourceStorageEntry[] = [];
   public storageFilesHashmap: Record<string, IResourceStorageEntry> = {};
-  public readonly resources = signal<IResourceRow[]>([]);
 
-  public get table() {
-    return this.supabaseService.db.table('resources');
+  public resources = {
+    collections: signal<IResourceCollectionRow[]>([]),
+    files: signal<IResourceFileRow[]>([]),
+    files_child: signal<IResourceFileChildRow[]>([]),
+    links: signal<IResourceLinkRow[]>([]),
+  };
+  public get tables() {
+    return {
+      collections: this.supabaseService.db.table('resource_collections'),
+      files: this.supabaseService.db.table('resource_files'),
+      files_child: this.supabaseService.db.table('resource_files_child'),
+      links: this.supabaseService.db.table('resource_links'),
+    };
   }
 
   constructor(
@@ -51,56 +60,6 @@ export class ResourcesDashboardService extends PicsaAsyncService {
     return this.storageFilesHashmap[id];
   }
 
-  /**
-   *
-   * TODO - only enable super admin/local dev
-   * TODO - remove when no longer required
-   */
-  public async migrateHardcodedResources() {
-    // NOTE - assumes storage files manually uploaded
-
-    // eslint-disable-next-line @nx/enforce-module-boundaries
-    const { DB_COLLECTION_ENTRIES, DB_FILE_ENTRIES, DB_LINK_ENTRIES } = await import(
-      '@picsa/resources/src/app/data/index'
-    );
-    console.log({ DB_COLLECTION_ENTRIES, DB_FILE_ENTRIES, DB_LINK_ENTRIES });
-    const ref = this.supabaseService.db.table('resources');
-    const uploaded: unknown[] = [];
-    const missing: unknown[] = [];
-
-    for (const fileEntry of Object.values(DB_FILE_ENTRIES)) {
-      const { type, description, url } = fileEntry;
-      // extract pathname from firebase url
-      const { pathname } = new URL(url);
-      const storagePath = decodeURI(pathname).replace(/%2F/g, '/').replace('/v0/b/picsa-apps.appspot.com/o/', '');
-      // check for equivalent storage file
-      const storageFile = this.storageFiles.find((file) => file.name === storagePath);
-      if (storageFile) {
-        const dbEntry: Database['public']['Tables']['resources']['Insert'] = {
-          description,
-          type,
-
-          storage_file: storageFile.id,
-        };
-        const { error } = await ref.upsert(dbEntry, { ignoreDuplicates: false });
-
-        if (error) {
-          console.error(error);
-        }
-        uploaded.push(fileEntry);
-      } else {
-        missing.push(fileEntry);
-      }
-    }
-    console.log({ uploaded, missing });
-    if (missing.length > 0) {
-      this.notificationService.showUserNotification({
-        matIcon: 'error',
-        message: `${missing.length} files missing from storage`,
-      });
-    }
-  }
-
   private async listStorageFiles() {
     const storageFiles = await this.storageService.list('resources');
     this.storageFiles = storageFiles.map((file) => ({
@@ -112,10 +71,14 @@ export class ResourcesDashboardService extends PicsaAsyncService {
   }
 
   private async listResources() {
-    const { data, error } = await this.supabaseService.db.table('resources').select<'*', IResourceRow>('*');
-    if (error) {
-      throw error;
-    }
-    this.resources.set(data);
+    const promises = Object.entries(this.tables).map(async ([name, table]) => {
+      const { data, error } = await table.select('*');
+      if (error) {
+        console.error(error);
+        this.notificationService.showUserNotification({ matIcon: 'error', message: error.message });
+      }
+      this.resources[name].set(data);
+    });
+    await Promise.all(promises);
   }
 }
