50cda9cbc2648a59f19d384f274f32ff47c3fd9a
Ochieng Paul <ochiengpaul442@gmail.com>
2024-04-18 19:07:33 +0300

    Add video playback schema and collection creator

---
+++ libs/shared/src/features/video-player/schema/schema.ts
@@ -0,0 +1,36 @@
+import type { IPicsaCollectionCreator } from '@picsa/shared/services/core/db_v2';
+import { RxJsonSchema } from 'rxdb';
+
+export interface IVideoPlayback {
+  videoId: string;
+  currentTime: number;
+  totalTime: number;
+  playbackPercentage: number;
+}
+
+export const videoPlaybackSchema: RxJsonSchema<IVideoPlayback> = {
+  title: 'video playback schema',
+  version: 0,
+  type: 'object',
+  primaryKey: 'videoId',
+  properties: {
+    videoId: {
+      type: 'string',
+    },
+    currentTime: {
+      type: 'number',
+    },
+    totalTime: {
+      type: 'number',
+    },
+    playbackPercentage: {
+      type: 'number',
+    },
+  },
+  required: ['videoId', 'currentTime', 'totalTime', 'playbackPercentage'],
+};
+
+export const videoPlayback: IPicsaCollectionCreator<IVideoPlayback> = {
+  schema: videoPlaybackSchema,
+  isUserCollection: true,
+};
