f016624b0544c99c23aad2ece3f525bf91656d4e
FaithDaka <faithbusigu8@gmail.com>
2024-04-22 08:12:02 +0300

    feat: drawing methods

--- libs/shared/src/features/drawing/drawing.component.ts
+++ libs/shared/src/features/drawing/drawing.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import {getStroke} from 'perfect-freehand'
+import { getStroke } from 'perfect-freehand';
 
 @Component({
   selector: 'picsa-custom-drawing',
@@ -9,4 +9,38 @@ import {getStroke} from 'perfect-freehand'
   templateUrl: './drawing.component.html',
   styleUrls: ['./drawing.component.scss'],
 })
-export class DrawingComponent {}
+export class DrawingComponent {
+  options = {
+    size: 32,
+    thinning: 0.5,
+    smoothing: 0.5,
+    streamline: 0.5,
+    easing: (t) => t,
+    start: {
+      taper: 0,
+      easing: (t) => t,
+      cap: true,
+    },
+    end: {
+      taper: 100,
+      easing: (t) => t,
+      cap: true,
+    },
+  };
+
+  getSvgPathFromStroke(stroke) {
+    if (!stroke.length) return '';
+
+    const d = stroke.reduce(
+      (acc, [x0, y0], i, arr) => {
+        const [x1, y1] = arr[(i + 1) % arr.length];
+        acc.push(x0, y0, (x0 + x1) / 2, (y0 + y1) / 2);
+        return acc;
+      },
+      ['M', ...stroke[0], 'Q']
+    );
+
+    d.push('Z');
+    return d.join(' ');
+  }
+}
