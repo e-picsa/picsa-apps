f103fee5ac13a93d20e59cce01622f5dad0f3ee5
Ochieng Paul <ochiengpaul442@gmail.com>
2024-05-12 23:29:08 +0300

    refactor: Update option list component with new option

--- apps/picsa-tools/option-tool/src/app/pages/option-list/option-list.component.ts
+++ apps/picsa-tools/option-tool/src/app/pages/option-list/option-list.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./option-list.component.scss'],
 })
 export class OptionListComponent {
-  options = ['crop', 'livestock', 'Livelihood'];
+  options = ['Crop', 'Livestock', 'Livelihood'];
 }
