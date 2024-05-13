c0fbf19528f8f473dc6edb0748821db235db554f
Chris <chris.m.clarke@live.co.uk>
2024-05-13 22:09:06 +0100

    fix: custom drawing render

--- libs/shared/src/features/drawing/drawing.component.ts
+++ libs/shared/src/features/drawing/drawing.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, signal, ViewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { getStroke } from 'perfect-freehand';
 
 @Component({
   selector: 'picsa-custom-drawing',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatDialogModule, MatButtonModule],
   templateUrl: './drawing.component.html',
   styleUrls: ['./drawing.component.scss'],
 })
@@ -27,19 +29,54 @@ export class PicsaDrawingComponent {
       cap: true,
     },
   };
-  public points: any[] = [];
-  public stroke;
-  public pathData;
+  /** [x,y,pressure] point representation */
+  public points: [number, number, number][] = [];
 
-  constructor() {
-    this.stroke = getStroke(this.points, this.options);
-    this.pathData = this.getSvgPathFromStroke(this.stroke);
+  /** SVG representation of active path segment */
+  public pathData = signal<string>('');
+
+  /**
+   * When the svg is rendered in a parent element track position
+   * to use to offset
+   */
+  private containerOffset = [0, 0];
+
+  @ViewChild('svgElement') svgElement: ElementRef<SVGElement>;
+
+  constructor(public dialog: MatDialog) {}
+
+  handlePointerDown(event: PointerEvent) {
+    const target = event.target as SVGElement;
+    target.setPointerCapture(event.pointerId);
+    this.calculateContainerOffset();
+    this.addPointToPath(event.pageX, event.pageY);
   }
 
-  public getSvgPathFromStroke(stroke) {
-    console.log('This is the stroke received:', stroke);
-    if (!stroke.length) return '';
+  handlePointerMove(event: PointerEvent) {
+    if (event.buttons !== 1) return;
+    this.addPointToPath(event.pageX, event.pageY);
+  }
 
+  private addPointToPath(x: number, y: number, pressure = 0.5) {
+    const [left, top] = this.containerOffset;
+    this.points.push([x - left, y - top, pressure]);
+    const stroke = getStroke(this.points);
+    const svgPath = this.getSvgPathFromStroke(stroke);
+    this.pathData.set(svgPath);
+  }
+
+  private calculateContainerOffset() {
+    const svgEl = this.svgElement.nativeElement;
+    const { left, top } = svgEl.getBoundingClientRect();
+    this.containerOffset = [left, top];
+  }
+
+  /**
+   * Generate an svg path from array of [x,y] point arrays
+   * Copied from https://github.com/steveruizok/perfect-freehand
+   * */
+  private getSvgPathFromStroke(stroke: number[][]) {
+    if (!stroke.length) return '';
     const d = stroke.reduce(
       (acc, [x0, y0], i, arr) => {
         const [x1, y1] = arr[(i + 1) % arr.length];
@@ -48,23 +85,7 @@ export class PicsaDrawingComponent {
       },
       ['M', ...stroke[0], 'Q']
     );
-
     d.push('Z');
-    console.log('Path:', d);
     return d.join(' ');
-  }
-
-  handlePointerDown(event) {
-    event.target.setPointerCapture(event.pointerId);
-    this.points = [[event.pageX, event.pageY, 0.5]];
-  }
-
-  handlePointerMove(event) {
-    if (event.buttons !== 1) return;
-    this.points = [...this.points, [event.pageX, event.pageY, 0.5]];
-    console.log('Points:', this.points);
-    this.stroke = getStroke(this.points, this.options);
-    console.log('Stroke:', this.stroke);
-    this.pathData = this.getSvgPathFromStroke(this.stroke);
   }
 }
