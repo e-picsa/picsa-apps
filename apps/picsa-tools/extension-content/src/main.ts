f3a8d3b90b61947e38820fae00386d8f1845f6fe
Chris <chris.m.clarke@live.co.uk>
2024-05-15 23:22:20 +0100

    chore: lint fixes

--- apps/picsa-tools/extension-content/src/main.ts
+++ apps/picsa-tools/extension-content/src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
-import { AppComponent } from './app/app.component';
+import { PicsaExtensionContent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+bootstrapApplication(PicsaExtensionContent, appConfig).catch((err) => console.error(err));
