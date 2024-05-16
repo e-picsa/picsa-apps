1e7242409ad8edcb8c0e04be76c21ea8b1e1a68d
Chris <chris.m.clarke@live.co.uk>
2024-05-16 11:35:55 +0100

    feat: add scrollRestore directive

---
+++ libs/shared/src/directives/scroll-restore.ts
@@ -0,0 +1,48 @@
+import { AfterViewInit, Directive, ElementRef, Injectable, OnDestroy, OnInit } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class ScrollRestoreService {
+  /** Saved scroll states for different location pathnames */
+  public savedStates: { [pathname: string]: number } = {};
+
+  public setPosition(pathname: string, scrollTop: number) {
+    this.savedStates[pathname] = scrollTop;
+  }
+
+  public getPosition(pathname: string) {
+    return this.savedStates[pathname];
+  }
+}
+
+/**
+ * Directive to restore an element's scrollTop position following navigation events
+ * This differs to angulars in-built scrollPositionRestoration router configuration which
+ * only applies to main body (whereas app content scrolls inside page-content div)
+ *
+ * The directive tracks scroll position for current page, and restores on revisit,
+ * E.g. if the user moves to a new page and then comes back
+ *
+ * @example
+ * <div class="page-content" scrollRestore>
+ * ...
+ * </div>
+ */
+@Directive({ selector: '[scrollRestore]', standalone: true })
+export class PicsaScrollRestoreDirective implements OnInit, AfterViewInit, OnDestroy {
+  private pathname: string;
+  constructor(private el: ElementRef<HTMLDivElement>, private service: ScrollRestoreService) {}
+
+  ngOnInit(): void {
+    this.pathname = location.pathname;
+  }
+  ngAfterViewInit() {
+    const savedPostion = this.service.getPosition(this.pathname);
+    if (savedPostion) {
+      this.el.nativeElement.scrollTop = savedPostion;
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.service.setPosition(this.pathname, this.el.nativeElement.scrollTop);
+  }
+}
