1cb022f57f117295f4a75c9b5aaef57bb0f669e3
Chris <chris.m.clarke@live.co.uk>
2024-05-22 13:17:42 +0100

    feat: add support for translation archive

--- apps/picsa-server/supabase/migrations/20240517095457_translations_table_columns.sql
+++ apps/picsa-server/supabase/migrations/20240517095457_translations_table_columns.sql
@@ -1,2 +1,5 @@
 alter table public.translations
-rename column en to global_en;
+rename column en to text
+add archived boolean
+;
+comment on column public.translations.archived is 'Specify whether translation string has been archived (retained for future use)';
