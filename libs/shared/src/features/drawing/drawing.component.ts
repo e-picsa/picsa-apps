2cc0b298a27d6356837eb804e5e1ac3e0b3e6a50
FaithDaka <faithbusigu8@gmail.com>
2024-05-27 09:50:57 +0300

    feat: undo, redo and clear draw functions

--- libs/shared/src/features/drawing/drawing.component.ts
+++ libs/shared/src/features/drawing/drawing.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, signal, ViewChild } from '@angular/core';
+import { UntypedFormBuilder } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { getStroke } from 'perfect-freehand';
@@ -24,7 +25,7 @@ export class PicsaDrawingComponent {
       cap: true,
     },
     end: {
-      taper: 100,
+      taper: 0,
       easing: (t) => t,
       cap: true,
     },
@@ -34,6 +35,10 @@ export class PicsaDrawingComponent {
 
   /** SVG representation of active path segment */
   public pathData = signal<string>('');
+
+  private undoStack: [number, number, number][][] = [];
+
+  private redoStack: [number, number, number][][] = [];
 
   /**
    * When the svg is rendered in a parent element track position
@@ -59,6 +64,11 @@ export class PicsaDrawingComponent {
     this.renderPath();
   }
 
+  handlePointerUp(event: PointerEvent) {
+    this._pushToUndo();
+    this._endCurrentStroke(event);
+  }
+
   /** Add a point to the current path, adjusting absolute position for relative container */
   private addPointToPath(x: number, y: number, pressure = 0.5) {
     const [left, top] = this.containerOffset;
@@ -77,6 +87,7 @@ export class PicsaDrawingComponent {
     const stroke = getStroke(this.points);
     const svgPath = this.getSvgPathFromStroke(stroke);
     this.pathData.set(svgPath);
+    this._pushToUndo();
   }
 
   /**
@@ -95,5 +106,62 @@ export class PicsaDrawingComponent {
     );
     d.push('Z');
     return d.join(' ');
+  }
+
+  /* Push to Undo Stack */
+  private _pushToUndo() {
+    this.undoStack.push([...this.points]);
+  }
+
+  /* Clear Draw */
+  clearDraw() {
+    this.points = [];
+    this.renderPath();
+  }
+
+  /* Undo previous Stroke render */
+  public undoSvgStroke() {
+    if (this.undoStack.length) {
+      const currentState = this.undoStack.pop();
+      if (currentState) {
+        this.redoStack.push(currentState);
+        this.points = currentState;
+        const stroke = getStroke(this.points);
+        const svgPath = this.getSvgPathFromStroke(stroke);
+        this.pathData.set(svgPath);
+      }
+    } else {
+      return;
+    }
+  }
+
+  /* Redo Stroke render */
+  public redoSvgStroke() {
+    if (this.redoStack.length) {
+      const currentState = this.redoStack.pop();
+      if (currentState) {
+        this.undoStack.push(currentState);
+        this.points = currentState;
+        const stroke = getStroke(this.points);
+        const svgPath = this.getSvgPathFromStroke(stroke);
+        this.pathData.set(svgPath);
+      }
+    } else {
+      return;
+    }
+  }
+
+  public _startNewStroke() {
+    if (this.points.length > 0) {
+      this._pushToUndo(); // Save the current stroke before starting a new one
+    }
+    this.points = []; // Clear the points for the new stroke
+  }
+
+  // Method to end the current stroke
+  public _endCurrentStroke(event: PointerEvent) {
+    // this.addPointToPath(event.pageX, event.pageY);
+    // this.points = null as never
+    // this.points.push([0, 0, 0])
   }
 }
