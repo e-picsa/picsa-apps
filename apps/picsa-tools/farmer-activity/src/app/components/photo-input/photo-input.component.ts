8176243349e882cc39a692855d3eeafeadfb0227
Ochieng Paul <ochiengpaul442@gmail.com>
2024-06-14 15:04:32 +0300

    chore: update server URL and resource metadata
    Update the server URL in the .env.local file to http://192.168.0.106:4200. Also, update the metadata for the resources in the contents.json file, including the md5Checksum and modifiedTime values.

---
+++ apps/picsa-tools/farmer-activity/src/app/components/photo-input/photo-input.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'farmer-activity-photo-input',
+  templateUrl: './photo-input.component.html',
+  styleUrl: './photo-input.component.scss',
+})
+export class PhotoInputComponent {}
