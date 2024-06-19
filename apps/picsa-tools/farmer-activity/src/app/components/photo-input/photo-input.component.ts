cfebb037837f10dda62383c147b04ba6a8d06e98
Ochieng Paul <ochiengpaul442@gmail.com>
2024-06-19 20:01:46 +0300

    chore: update server URL to http://192.168.0.105:4200 and add photo upload functionality to activity details page

--- apps/picsa-tools/farmer-activity/src/app/components/photo-input/photo-input.component.ts
+++ apps/picsa-tools/farmer-activity/src/app/components/photo-input/photo-input.component.ts
@@ -1,8 +1,59 @@
-import { Component } from '@angular/core';
+import { Platform } from '@angular/cdk/platform';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+
+// Temporary service to simulate the photo service that will handle the photo input and output.
+import { PhotoService } from '../../services/photo-input.service';
+
+interface Photo {
+  webPath?: string;
+}
 
 @Component({
   selector: 'farmer-activity-photo-input',
   templateUrl: './photo-input.component.html',
-  styleUrl: './photo-input.component.scss',
+  styleUrls: ['./photo-input.component.scss'],
 })
-export class PhotoInputComponent {}
+export class PhotoInputComponent implements OnInit {
+  @ViewChild('fileInput') fileInput: ElementRef;
+  photos: Photo[] = [];
+  isWebPlatform: boolean;
+
+  constructor(private platform: Platform, private photoService: PhotoService) {}
+
+  ngOnInit() {
+    this.isWebPlatform = this.platform.isBrowser;
+  }
+
+  async takeOrChoosePicture() {
+    const source = this.isWebPlatform ? CameraSource.Photos : CameraSource.Prompt;
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri,
+      source: source,
+    });
+
+    this.photos.push({
+      webPath: image.webPath,
+    });
+  }
+
+  processWebPicture(event: any) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const photo = { webPath: e.target?.result as string };
+      this.photos.push(photo);
+    };
+    reader.readAsDataURL(file);
+  }
+
+  removePhoto(index: number) {
+    this.photos.splice(index, 1);
+  }
+
+  removeAllPhotos() {
+    this.photos = [];
+  }
+}
