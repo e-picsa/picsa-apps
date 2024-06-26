7968b5387bfc2bdb5e9384ce294f16fbbd57b3ad
FaithDaka <faithbusigu8@gmail.com>
2024-06-26 12:12:09 +0300

    feat: audio playback component

--- libs/shared/src/features/audio-playback/audio-playback.component.ts
+++ libs/shared/src/features/audio-playback/audio-playback.component.ts
@@ -1,27 +1,36 @@
-import { Component, Input } from '@angular/core';
-
+import { Component, ViewChild, ElementRef, OnDestroy, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { AudioService } from './audio-playback.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'picsa-audio-playback',
   standalone: true,
+  imports:[CommonModule, MatIconModule],
   templateUrl: './audio-playback.component.html',
   styleUrls: ['./audio-playback.component.scss'],
 })
-export class AudioPlaybackComponent {
+export class AudioPlaybackComponent implements OnDestroy {
   @Input() audioUrl: string;
+  isAudioPlaying = false;
 
   constructor(private audioService: AudioService) {}
 
   togglePlayback(): void {
     if (this.audioService.isPlaying()) {
       this.audioService.pauseAudio();
+      this.isAudioPlaying = true;
     } else {
       this.audioService.playAudio(this.audioUrl);
+      this.isAudioPlaying = false;
     }
   }
 
   isPlaying(): boolean {
     return this.audioService.isPlaying();
+  }
+
+  ngOnDestroy(): void {
+    this.audioService.stop();
   }
 }
