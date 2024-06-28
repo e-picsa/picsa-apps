b1d36dbe5bc2b56b34d8f882830034217a59b714
Chris <chris.m.clarke@live.co.uk>
2024-06-28 12:43:09 +0100

    feat: add updated content icons and steps

--- libs/data/farmer_content/index.ts
+++ libs/data/farmer_content/index.ts
@@ -39,50 +39,64 @@ interface IFarmerContentStepVideo {
 }
 
 const CONTENT_STEPS: { [id in IFarmerContentId]: any } = {
-  climate_change: [],
-  compare_options: [],
-  intro: [],
-  opportunities_risks: [],
-  what_are_the_options: [],
-  what_does_the_farmer_do: [],
+  '0_intro': [],
+  '1_what_you_do': [],
+  '2_climate_change': [],
+  '3_opportunities_risks': [],
+  '4_what_are_the_options': [],
+  '5_compare_options': [],
+  '6_decide_and_plan': [],
+  '7_use_forecasts': [],
 };
 
 const { seasonal_calendar, resource_allocation_map, climate, probability_and_risk, options } = TOOLS_DATA_HASHMAP;
 
 const FARMER_CONTENT_BASE = {
-  intro: {
+  '0_intro': {
     slug: 'intro',
     title: translateMarker('What is PICSA?'),
     tools: [],
     tags: [{ label: translateMarker('Tutorials') }],
   },
-  what_does_the_farmer_do: {
+  '1_what_you_do': {
     slug: 'what-do-you-currently-do',
     title: translateMarker('What do you currently do?'),
     tools: [seasonal_calendar, resource_allocation_map],
     tags: [],
   },
-  climate_change: {
+  '2_climate_change': {
     slug: 'is-the-climate-changing',
-    title: translateMarker('Is your climate changing?'),
+    title: translateMarker('What is happening to the climate in your area?'),
     tools: [climate],
     tags: [],
   },
-  opportunities_risks: {
+  '3_opportunities_risks': {
     slug: 'opportunities-and-risk',
     title: translateMarker('What are the opportunities and risk?'),
     tools: [probability_and_risk],
     tags: [],
   },
-  what_are_the_options: {
+  '4_what_are_the_options': {
     slug: 'what-are-the-options',
     title: translateMarker('What changes can you make?'),
     tools: [options],
     tags: [],
   },
-  compare_options: {
+  '5_compare_options': {
     slug: 'compare-options',
     title: translateMarker('Are the changes a good idea?'),
+    tools: [],
+    tags: [],
+  },
+  '6_decide_and_plan': {
+    slug: 'decide-and-plan',
+    title: translateMarker('You decide and make a plan'),
+    tools: [],
+    tags: [],
+  },
+  '7_use_forecasts': {
+    slug: 'use-forecasts',
+    title: translateMarker('Use the forecasts to update and adapt your plans'),
     tools: [],
     tags: [],
   },
