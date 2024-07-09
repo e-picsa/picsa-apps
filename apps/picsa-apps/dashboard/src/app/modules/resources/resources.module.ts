a752f02ddb86189bd44c82f784c2a6ffb966167e
chrismclarke <chris.m.clarke@live.co.uk>
2024-07-09 14:14:56 -0700

    chore: update default resource landing page

--- apps/picsa-apps/dashboard/src/app/modules/resources/resources.module.ts
+++ apps/picsa-apps/dashboard/src/app/modules/resources/resources.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { ResourceCollectionsComponent } from './pages/collections/resource-collections.component';
-import { ResourceCreateComponent } from './pages/create/resource-create.component';
+import { ResourceFileEditComponent } from './pages/files/edit/resource-file-edit.component';
 import { ResourceFilesComponent } from './pages/files/resource-files.component';
 import { ResourceLinksComponent } from './pages/links/resource-links.component';
 
@@ -14,30 +14,29 @@ import { ResourceLinksComponent } from './pages/links/resource-links.component';
     RouterModule.forChild([
       {
         path: '',
-        redirectTo: 'collections',
+        redirectTo: 'files',
         pathMatch: 'full',
-      },
-      {
-        path: 'collections',
-        component: ResourceCollectionsComponent,
       },
       {
         path: 'files',
         component: ResourceFilesComponent,
       },
-
       {
         path: 'links',
         component: ResourceLinksComponent,
       },
+      {
+        path: 'collections',
+        component: ResourceCollectionsComponent,
+      },
 
       {
         path: 'files/create',
-        component: ResourceCreateComponent,
+        component: ResourceFileEditComponent,
       },
       {
         path: 'files/:id',
-        component: ResourceCreateComponent,
+        component: ResourceFileEditComponent,
       },
     ]),
   ],
