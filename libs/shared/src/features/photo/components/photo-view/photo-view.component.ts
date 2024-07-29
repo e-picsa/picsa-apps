b13bb6e7e01b9462bda9268e5454fb5d0d3e8617
Ochieng Paul <ochiengpaul442@gmail.com>
2024-07-29 12:18:16 +0300

    chore: Update photo-view component with dialog template and mobile responsiveness

--- libs/shared/src/features/photo/components/photo-view/photo-view.component.ts
+++ libs/shared/src/features/photo/components/photo-view/photo-view.component.ts
@@ -1,5 +1,7 @@
-import { Component, effect, input, signal } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, effect, Input, signal, TemplateRef,ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 
 import { PicsaDialogService } from '../../../dialog';
@@ -9,27 +11,35 @@ import { IPhotoEntry } from '../../schema';
 @Component({
   selector: 'picsa-photo-view',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, MatDialogModule],
   templateUrl: './photo-view.component.html',
-  styleUrl: './photo-view.component.scss',
+  styleUrls: ['./photo-view.component.scss'],
 })
 export class PhotoViewComponent {
   /** Input photo document ref */
-  photo = input.required<IPhotoEntry>();
+  @Input() photo!: IPhotoEntry;
   /** Path to resource for render */
   uri = signal('');
   /** Error message to display */
   errorMsg = signal('');
+  isMobile = signal(false);
 
-  constructor(private service: PhotoService, private dialog: PicsaDialogService) {
+  @ViewChild('dialogTemplate') dialogTemplate!: TemplateRef<any>;
+
+  constructor(
+    private service: PhotoService,
+    private dialog: PicsaDialogService,
+    public photoDialog: MatDialog,
+    private breakpointObserver: BreakpointObserver
+  ) {
     effect(
       async (onCleanup) => {
-        const photo = this.photo();
+        const photo = this.photo;
         const uri = await this.service.getPhotoAttachment(photo.id);
         if (uri) {
           this.uri.set(uri);
         } else {
-          console.error('[Photo] not found', this.photo());
+          console.error('[Photo] not found', this.photo);
           this.errorMsg.set(`Photo not found`);
         }
         onCleanup(() => {
@@ -38,13 +48,25 @@ export class PhotoViewComponent {
       },
       { allowSignalWrites: true }
     );
+
+    this.breakpointObserver.observe([Breakpoints.Handset]).subscribe((result) => {
+      this.isMobile.set(result.matches);
+    });
+  }
+
+  openPhotoDialog() {
+    this.photoDialog.open(this.dialogTemplate, {
+      data: { photo: this.photo, uri: this.uri() },
+      panelClass: this.isMobile() ? 'full-screen-dialog' : '',
+    });
   }
 
   public async promptDelete() {
     const dialogRef = await this.dialog.open('delete');
     dialogRef.afterClosed().subscribe(async (shouldDelete) => {
       if (shouldDelete) {
-        await this.service.deletePhoto(this.photo().id);
+        await this.service.deletePhoto(this.photo.id);
+        this.photoDialog.closeAll();
       }
     });
   }
