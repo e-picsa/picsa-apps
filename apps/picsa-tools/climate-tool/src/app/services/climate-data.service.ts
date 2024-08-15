fceb3e9e0c9107abda006c39ee3891544c528e3f
chrismclarke <chris.m.clarke@live.co.uk>
2024-08-14 21:52:19 -0700

    refactor: computed station lists

--- apps/picsa-tools/climate-tool/src/app/services/climate-data.service.ts
+++ apps/picsa-tools/climate-tool/src/app/services/climate-data.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { computed, Injectable } from '@angular/core';
+import { ConfigurationService } from '@picsa/configuration';
 import { IChartMeta, IStationData, IStationMeta } from '@picsa/models';
 import { arrayToHashmap, loadCSV } from '@picsa/utils';
 
@@ -9,14 +10,26 @@ export class ClimateDataService {
   public activeChart: IChartMeta;
   public yValues: number[];
 
-  public dataByStation: { [name: string]: IStationMeta } = {};
+  /** Store populated station data to in-memory cache for fast future retrieval*/
+  private stationDataCache: { [name: string]: IStationMeta } = {};
 
-  constructor() {
-    this.dataByStation = arrayToHashmap(DATA.HARDCODED_STATIONS, 'id');
+  /** List of all stations for current  */
+  public stations = computed(() => {
+    const { climateTool, country_code } = this.configurationService.deploymentSettings();
+    const filterFn = climateTool?.station_filter;
+    if (filterFn) {
+      return DATA.HARDCODED_STATIONS.filter((station) => filterFn(station));
+    } else {
+      return DATA.HARDCODED_STATIONS.filter((station) => station.countryCode === country_code);
+    }
+  });
+
+  constructor(private configurationService: ConfigurationService) {
+    this.stationDataCache = arrayToHashmap(DATA.HARDCODED_STATIONS, 'id');
   }
 
   public async getStationMeta(stationID: string): Promise<IStationMeta> {
-    const station = this.dataByStation[stationID];
+    const station = this.stationDataCache[stationID];
     if (!station) {
       console.error('No data for station');
       return { data: [] as any[], name: 'Data not found' } as IStationMeta;
@@ -24,13 +37,12 @@ export class ClimateDataService {
     if (!station.data) {
       console.log('[Climate] Load Data', stationID);
       const summaries = await this.loadStationSummaries(stationID);
-      this.dataByStation[stationID].data = summaries;
+      this.stationDataCache[stationID].data = summaries;
     }
     // HACK - ensure chart definitions don't persist across sites
-    return JSON.parse(JSON.stringify(this.dataByStation[stationID]));
+    return JSON.parse(JSON.stringify(this.stationDataCache[stationID]));
   }
   private async loadStationSummaries(stationID: string) {
-    // TODO - ensure still working
     return loadCSV<IStationData>(`assets/summaries/${stationID}.csv`, {
       download: true,
       dynamicTyping: true,
