c500272a1d455510f7d6c09917c2c0779a286305
chrismclarke <chris.m.clarke@live.co.uk>
2024-08-14 21:52:37 -0700

    chore: disable tj country data

--- libs/data/deployments/countries.ts
+++ libs/data/deployments/countries.ts
@@ -15,11 +15,14 @@ const COUNTRIES_BASE: { [key in ICountryCode]: { label: string } } = {
   tj: { label: 'Tajikistan' },
 };
 
-export const COUNTRIES_DATA = Object.entries(COUNTRIES_BASE).map(([id, { label }]) => ({
-  id: id as ICountryCode,
-  label,
-  flag_path: `assets/images/flags/${id}.svg`,
-}));
+export const COUNTRIES_DATA = Object.entries(COUNTRIES_BASE)
+  .map(([id, { label }]) => ({
+    id: id as ICountryCode,
+    label,
+    flag_path: `assets/images/flags/${id}.svg`,
+  }))
+  // HACK - remove Tajikistan data as not currently in use
+  .filter((c) => c.id !== 'tj');
 export type ICountriesDataEntry = typeof COUNTRIES_DATA[0];
 export const COUNTRIES_DATA_HASHMAP = arrayToHashmap(COUNTRIES_DATA, 'id') as {
   [code in ICountryCode]: ICountriesDataEntry;
