880fa38a3e242140f5365c27f960bf10ebd88bd7
chrismclarke <chris.m.clarke@live.co.uk>
2024-08-14 22:28:53 -0700

    chore: clean missing values

--- apps/picsa-tools/climate-tool/src/app/services/climate-data.service.ts
+++ apps/picsa-tools/climate-tool/src/app/services/climate-data.service.ts
@@ -37,16 +37,43 @@ export class ClimateDataService {
     if (!station.data) {
       console.log('[Climate] Load Data', stationID);
       const summaries = await this.loadStationSummaries(stationID);
-      this.stationDataCache[stationID].data = summaries;
+      const cleaned = this.hackCleanMissingValues(summaries);
+      this.stationDataCache[stationID].data = cleaned;
     }
     // HACK - ensure chart definitions don't persist across sites
     return JSON.parse(JSON.stringify(this.stationDataCache[stationID]));
   }
+
+  /**
+   * Remove data entries where 1 of start, end, length or rainfall not defined
+   * This may be a null value, or 0 entry
+   */
+  private hackCleanMissingValues(data: IStationData[]) {
+    return data.map((el) => {
+      const { Start, End, Rainfall, Length } = el;
+      const isMissingData = !Start || !End || !Rainfall || !Length;
+      if (isMissingData) {
+        el.Start = null as any;
+        el.End = null as any;
+        el.Length = null as any;
+        el.Rainfall = null as any;
+      }
+      return el;
+    });
+  }
+
   private async loadStationSummaries(stationID: string) {
     return loadCSV<IStationData>(`assets/summaries/${stationID}.csv`, {
       download: true,
       dynamicTyping: true,
       header: true,
+      transform: (v) => {
+        // Ensure null string parsed
+        if (v === 'null') return null;
+        // HACK - replace 0 with null value
+        if (v === '0') return null;
+        return v;
+      },
     });
   }
 }
