c7c6d6a7c351533e2df24b6c514cdbbd1aa731a6
Chris <chris.m.clarke@live.co.uk>
2024-10-14 21:55:07 +0100

    feat: forecasts data

---
+++ libs/data/climate/forecasts/index.ts
@@ -0,0 +1,5 @@
+import zm from './zm';
+
+import mw from './mw';
+
+export const CLIMATE_FORECASTS = { zm, mw };
---
+++ libs/data/climate/forecasts/zm.ts
@@ -0,0 +1,25 @@
+const seasonal = {
+  id: 'c139d571-723c-4013-8246-28b0a291becf',
+  bucket_id: 'zm',
+  name: 'forecasts/ZMD Seasonal forecast 2024-25.pdf',
+  owner: '',
+  created_at: '2024-10-14 18:12:29.879524+00',
+  updated_at: '2024-10-14 18:12:29.879524+00',
+  last_accessed_at: '2024-10-14 18:12:29.879524+00',
+  metadata: {
+    eTag: 'cc62ececec1d335da2fe7230c90f1a1e-1',
+    size: 2111455,
+    mimetype: 'application/pdf',
+    cacheControl: 'max-age=3600',
+    lastModified: '2024-10-14T18:12:30.000Z',
+    contentLength: 2111455,
+    httpStatusCode: 200,
+  },
+  path_tokens: ['forecasts', 'ZMD Seasonal forecast 2024-25.pdf'],
+  version: 'f5342a2a-55fb-48be-acd3-b2b82a581c5c',
+  owner_id: '',
+};
+
+export const downscaled = [];
+
+export default { seasonal };
