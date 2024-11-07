79f55473a1660433d0620a5beab479154ffb4445
FaithDaka <faithbusigu8@gmail.com>
2024-11-07 10:51:03 +0300

    refactor: audio play logic

--- libs/shared/src/features/audio-playback/audio-playback.component.ts
+++ libs/shared/src/features/audio-playback/audio-playback.component.ts
@@ -1,12 +1,13 @@
-import { Component, ViewChild, ElementRef, OnDestroy, Input } from '@angular/core';
+import { Component, OnDestroy, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AudioService } from './audio-playback.service';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'picsa-audio-playback',
   standalone: true,
-  imports:[CommonModule, MatIconModule],
+  imports:[CommonModule, MatIconModule, MatButtonModule],
   templateUrl: './audio-playback.component.html',
   styleUrls: ['./audio-playback.component.scss'],
 })
@@ -17,17 +18,12 @@ export class AudioPlaybackComponent implements OnDestroy {
   constructor(private audioService: AudioService) {}
 
   togglePlayback(): void {
+    this.isAudioPlaying = !this.isAudioPlaying;
     if (this.audioService.isPlaying()) {
       this.audioService.pauseAudio();
-      this.isAudioPlaying = true;
     } else {
       this.audioService.playAudio(this.audioUrl);
-      this.isAudioPlaying = false;
     }
-  }
-
-  isPlaying(): boolean {
-    return this.audioService.isPlaying();
   }
 
   ngOnDestroy(): void {
