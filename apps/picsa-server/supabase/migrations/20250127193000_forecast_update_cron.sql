7ebce03669d7df63d42ebd66852ee205fb113bcf
chrismclarke <chris.m.clarke@live.co.uk>
2025-01-27 22:34:02 -0800

    feat: edge function utils

--- apps/picsa-server/supabase/migrations/20250127193000_forecast_update_cron.sql
+++ apps/picsa-server/supabase/migrations/20250127193000_forecast_update_cron.sql
@@ -7,19 +7,17 @@ grant all privileges on all tables in schema cron to postgres;
 -- enable the "pg_net" extension.
 create extension if not exists pg_net;
 
+-- Create function to trigger functions endoint for specific country codes
 -- https://supabase.com/docs/guides/functions/schedule-functions
 create or replace function public.climate_forecast_update() 
-returns bigint 
+returns bigint[] 
 language plpgsql 
 as $$ 
 
 declare
-    -- url:='https://project-ref.supabase.co/functions/v1/function-name'
-    url text ='http://host.docker.internal:54321/functions/v1/dashboard/climate-forecast-update';
-    headers jsonb ='{"Content-Type": "application/json"}'::jsonb;
     body jsonb;
-
-    request_id BIGINT;
+    request_id bigint;
+    request_ids BIGINT[] = array[]::BIGINT[];
     -- TODO - lookup from db
     country_codes text[] = array['mw','zm'];
     country_code text;
@@ -27,18 +25,23 @@ declare
 begin 
  foreach country_code in array country_codes loop
     body = format('{"country_code": "%s"}',country_code)::jsonb;
-    perform (net.http_post(url:=url,headers:=headers,body:=body));
+    select (public.call_edge_function('dashboard/climate-forecast-update',body)) into request_id;
+    request_ids = request_ids || request_id;    
   end loop;
-  return 0;
+  -- Requests will be handled asynchronously, so for now just return callback list of ids triggered   
+  return 'request_ids';
 end $$; 
 
-select public.climate_forecast_update();
 
 -- Trigger to run every hour at the 30 minute mark
 select cron.schedule('climate_forecasts_update', '30 * * * *', 'CALL climate_forecast_update()');
 
+-- TODO - could maybe just use cron schedule and ignore individual country code lookups
+
 
 -- Test via sql:
---select public.climate_forecast_update();
+-- select public.climate_forecast_update();
 -- (check for update)
--- select * from net._http_response where id=xx;
+-- select * from net._http_response order by id desc limit 5;
+
+-- SELECT public.call_edge_function('dashboard/climate-forecast-update', '{"country_code":"mw"}'::jsonb);
--- apps/picsa-server/supabase/seed.sql
+++ apps/picsa-server/supabase/seed.sql
@@ -2,3 +2,11 @@
 -- https://github.com/orgs/supabase/discussions/9314
 
 -- COPY climate_stations FROM 'data/climate_stations_rows.csv' WITH (FORMAT csv);
+
+-- Assign the local docker project-url for use in functions
+-- NOTE - should be manually set on production
+select vault.create_secret(
+  'http://host.docker.internal:54321',
+  'project_url',
+  'supabase project url'
+);
