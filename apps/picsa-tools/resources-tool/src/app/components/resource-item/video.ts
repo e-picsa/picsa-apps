46ab0ab6bd372b99b7fcd4e8ff320fa6aca7b522
FaithDaka <faithbusigu8@gmail.com>
2025-02-04 11:17:27 +0300

    select dropdown for video languages

--- apps/picsa-tools/resources-tool/src/app/components/resource-item/video.ts
+++ apps/picsa-tools/resources-tool/src/app/components/resource-item/video.ts
@@ -11,6 +11,12 @@ import { IResourceFile } from '../../schemas';
     <picsa-video-player [source]="fileURI" #videoPlayer [thumbnail]="resource.cover?.image" [id]="resource.id">
     </picsa-video-player>
     <p *ngIf="resource.description">{{ resource.description | translate }}</p>
+    <mat-form-field style="width: 80%; margin:3px 0 0 3px" class="no-hint" data-tour-id="resource-item-video">
+      <mat-label>{{ 'Language' | translate }}</mat-label>
+      <mat-select [(value)]="resource.language">
+        <mat-option *ngFor="let option of resource.languages" [value]="option">{{ option }}</mat-option>
+      </mat-select>
+    </mat-form-field>
   `,
   standalone: false,
 })
