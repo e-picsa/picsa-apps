782bcbdc4237750555e863c4c8982632a8520e24
chrismclarke <chris.m.clarke@live.co.uk>
2025-02-06 10:23:32 -0800

    feat: cliamte forecasts app schema

---
+++ apps/picsa-tools/climate-tool/src/app/pages/forecast/schemas/index.ts
@@ -0,0 +1,6 @@
+import * as schema from './schema_v0';
+
+// Re-export schema to provide latest version without need to refactor additonal code
+
+export const CLIMATE_FORECAST_COLLECTION = schema.COLLECTION_V0;
+export const CLIMATE_FORECAST_SCHEMA = schema.SCHEMA_V0;
---
+++ apps/picsa-tools/climate-tool/src/app/pages/forecast/schemas/schema_v0.ts
@@ -0,0 +1,33 @@
+import type { IPicsaCollectionCreator } from '@picsa/shared/services/core/db_v2';
+import { RxJsonSchema } from 'rxdb';
+
+import { IClimateForecastRow } from '../forecast.types';
+
+const SCHEMA_VERSION = 0;
+
+export const SCHEMA_V0: RxJsonSchema<IClimateForecastRow> = {
+  title: 'climate_forecasts',
+  version: SCHEMA_VERSION,
+  keyCompression: false,
+  type: 'object',
+  properties: {
+    country_code: { type: 'string' },
+    created_at: { type: 'string' },
+    forecast_type: { type: 'string' },
+    id: { type: 'string' },
+    language_code: { type: 'string' },
+    location: { type: 'string' },
+    mimetype: { type: 'string' },
+    storage_file: { type: 'string' },
+    updated_at: { type: 'string' },
+  },
+  required: ['id'],
+  primaryKey: 'id',
+};
+
+export const COLLECTION_V0: IPicsaCollectionCreator<IClimateForecastRow> = {
+  schema: SCHEMA_V0,
+  isUserCollection: false,
+  migrationStrategies: {},
+  attachments: {},
+};
