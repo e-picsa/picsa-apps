67b774709262d1984bbb717df3237f8daf80f62d
chrismclarke <chris.m.clarke@live.co.uk>
2025-02-13 11:44:03 -0800

    feat: wip forecast page

--- apps/picsa-tools/climate-tool/src/app/pages/forecast/forecast.page.ts
+++ apps/picsa-tools/climate-tool/src/app/pages/forecast/forecast.page.ts
@@ -1,14 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { Component, effect, signal } from '@angular/core';
+import { Component, computed, effect, OnDestroy, signal, viewChildren } from '@angular/core';
 import { ConfigurationService } from '@picsa/configuration';
-import { ICountryCode } from '@picsa/data';
 import { ResourcesComponentsModule } from '@picsa/resources/src/app/components/components.module';
 import { IResourceFile } from '@picsa/resources/src/app/schemas';
 import { PdfViewerComponent } from '@picsa/shared/features';
 import { PicsaTranslateModule } from '@picsa/shared/modules';
-import { SupabaseStorageService } from '@picsa/shared/services/core/supabase/services/supabase-storage.service';
+import { SupabaseStorageDownloadComponent } from '@picsa/shared/services/core/supabase';
+import { RxDocument } from 'rxdb';
 
 import { ClimateToolComponentsModule } from '../../components/climate-tool-components.module';
+import { ClimateForecastService } from './forecast.service';
+import { IClimateForecastRow } from './forecast.types';
+
+interface IForecastSummary {
+  _doc: RxDocument<IClimateForecastRow>;
+  id: string;
+  label: string;
+  downloaded: boolean;
+  storage_file: string | null;
+}
 
 @Component({
   selector: 'climate-forecast',
@@ -20,39 +30,80 @@ import { ClimateToolComponentsModule } from '../../components/climate-tool-compo
     PicsaTranslateModule,
     PdfViewerComponent,
     ResourcesComponentsModule,
+    SupabaseStorageDownloadComponent,
   ],
 })
-export class ClimateForecastComponent {
+export class ClimateForecastComponent implements OnDestroy {
   forecastTypes = ['Annual', 'Downscaled'];
-  public page = 1;
   public pdfSrc?: string;
+
+  private downloaders = viewChildren(SupabaseStorageDownloadComponent);
+
+  public dailyForecasts = computed(() =>
+    this.service.dailyForecastDocs().map((doc): IForecastSummary => {
+      const { id, storage_file } = doc;
+      const label = this.generateForecastLabel(id);
+      const summary: IForecastSummary = { _doc: doc, id, label, storage_file, downloaded: false };
+      if (storage_file) {
+        summary.downloaded = doc.getAttachment(storage_file) ? true : false;
+      }
+      return summary;
+    })
+  );
 
   public seasonalForecasts = signal<IResourceFile[]>([]);
 
   public downscaledForecasts = signal<IResourceFile[]>([]);
 
-  constructor(private configurationService: ConfigurationService, private storageService: SupabaseStorageService) {
-    effect(
-      () => {
-        const { country_code } = this.configurationService.deploymentSettings();
-        this.loadForecasts(country_code);
-      },
-      { allowSignalWrites: true }
-    );
+  constructor(private service: ClimateForecastService, private configurationService: ConfigurationService) {
+    effect(() => {
+      const { country_code } = this.configurationService.deploymentSettings();
+      if (country_code) {
+        this.service.loadDailyForecasts(country_code);
+      }
+    });
   }
 
-  private loadForecasts(country_code: ICountryCode) {
-    // TODO
+  ngOnDestroy() {
+    this.closeForecast();
   }
 
-  // ngOnInit() {}
-  openAnnualForeCast() {
-    this.pdfSrc = '/assets/forecast-assets/forecastDoc.pdf';
+  public async handleForecastClick(forecast: IForecastSummary) {
+    if (forecast.downloaded) {
+      this.openForecast(forecast);
+    } else {
+      const downloader = this.downloaders().find((d) => d.storage_path() === forecast.storage_file);
+      if (downloader) {
+        forecast._doc = await this.service.downloadForecastFile(forecast._doc, downloader);
+        await this.openForecast(forecast);
+      }
+    }
   }
-  openDownscaledForeCast() {
-    this.pdfSrc = '/assets/forecast-assets/forecastDoc.pdf';
-  }
-  clearPdf() {
+
+  public closeForecast() {
+    if (this.pdfSrc) {
+      URL.revokeObjectURL(this.pdfSrc);
+    }
     this.pdfSrc = undefined;
+  }
+
+  private async openForecast(forecast: IForecastSummary) {
+    const attachment = forecast._doc.getAttachment(forecast.storage_file as string);
+    if (!attachment) {
+      throw new Error('Forecast file not found');
+    }
+    const blob = await attachment.getData();
+    // TODO - handle html (maybe general file-viewer component)
+    const uri = URL.createObjectURL(blob);
+    this.pdfSrc = uri;
+  }
+
+  private generateForecastLabel(id: string) {
+    const [timestamp, filename] = id.split('/');
+    if (filename) {
+      const [basename, extension] = filename.split('.');
+      return basename.replace(/-/g, ' ');
+    }
+    return id;
   }
 }
