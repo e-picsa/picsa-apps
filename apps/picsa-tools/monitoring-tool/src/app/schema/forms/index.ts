359baca1901a866d3225887bf1956337486d4637
chrismclarke <chris.m.clarke@live.co.uk>
2025-02-13 18:40:14 -0800

    chore: revert schema changes

--- apps/picsa-tools/monitoring-tool/src/app/schema/forms/index.ts
+++ apps/picsa-tools/monitoring-tool/src/app/schema/forms/index.ts
@@ -1,7 +1,7 @@
-import { COLLECTION_V4, IMonitoringForm_V4, SCHEMA_V4 } from './schema_V4';
+import { COLLECTION_V3, IMonitoringForm_V3, SCHEMA_V3 } from './schema_V3';
 
 // Re-export schema to provide latest version without need to refactor additonal code
 
-export const COLLECTION = COLLECTION_V4;
-export type IMonitoringForm = IMonitoringForm_V4;
-export const SCHEMA = SCHEMA_V4;
+export const COLLECTION = COLLECTION_V3;
+export type IMonitoringForm = IMonitoringForm_V3;
+export const SCHEMA = SCHEMA_V3;
--- libs/shared/src/services/core/db_v2/schemas/attachments/index.ts
+++ libs/shared/src/services/core/db_v2/schemas/attachments/index.ts
@@ -1,7 +1,7 @@
-import * as schema from './schema_V1';
+import * as schema from './schema_v0';
 
 // Re-export schema to provide latest version without need to refactor additonal code
 
-export const ATTACHMENTS_COLLECTION = schema.COLLECTION_V1;
-export type IAttachment = schema.IAttachment_V1;
-export const ATTACHMENTS_SCHEMA = schema.SCHEMA_V1;
+export const ATTACHMENTS_COLLECTION = schema.COLLECTION_V0;
+export type IAttachment = schema.IAttachment_v0;
+export const ATTACHMENTS_SCHEMA = schema.SCHEMA_V0;
