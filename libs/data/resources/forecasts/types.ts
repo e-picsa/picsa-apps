3c8cdb4a714c8e38bdb1eeb71b2268a2265506f9
chrismclarke <chris.m.clarke@live.co.uk>
2025-02-26 09:54:22 -0800

    chore: remove legacy forecast resources

--- libs/data/resources/forecasts/types.ts
+++
@@ -1,6 +0,0 @@
-import type { Database } from '@picsa/server-types';
-
-// TODO - use general storage entry
-export type IForecastStorageEntry = Database['public']['Views']['storage_objects']['Row'];
-
-export type IForecasts = { seasonal: IForecastStorageEntry[]; downscaled: IForecastStorageEntry[] };
