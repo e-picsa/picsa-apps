fa855f964c6dc88445902a8af94f3e036d53df93
chrismclarke <chris.m.clarke@live.co.uk>
2025-02-28 21:06:26 -0800

    fix: pdf viewer compatibility

--- libs/shared/src/features/pdf-viewer/pdf-viewer.component.ts
+++ libs/shared/src/features/pdf-viewer/pdf-viewer.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { Capacitor } from '@capacitor/core';
-import { pdfDefaultOptions } from 'ngx-extended-pdf-viewer';
+import { pdfDefaultOptions, PDFScriptLoaderService } from 'ngx-extended-pdf-viewer';
 import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 
 @Component({
@@ -9,7 +10,7 @@ import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
   templateUrl: './pdf-viewer.component.html',
   styleUrls: ['./pdf-viewer.component.scss'],
   encapsulation: ViewEncapsulation.None,
-  imports: [NgxExtendedPdfViewerModule, CommonModule],
+  imports: [NgxExtendedPdfViewerModule, MatButtonModule, CommonModule],
 })
 export class PdfViewerComponent {
   legacyBrowser = true;
@@ -22,30 +23,25 @@ export class PdfViewerComponent {
   constructor() {
     // name of folder pdf viewer assets copied to as declared in `angular.json`
     pdfDefaultOptions.assetsFolder = 'assets/pdf-viewer';
-    this.legacyBrowser = isLegacyBrowser();
+    // force viewer to not use es5 fallback (not included in build)
+    // use comparable check below to share message if not available for legacy browser
+    this.runCompatibilityCheck();
   }
-}
 
-/**
- * Check whether browser supports post-ES5 features. Further checks could be made, for example
- * whether the browser is IE/iOS, but not necessary at runtime as these platforms aren't supported anyway.
- * See discussion thread here: https://github.com/IDEMSInternational/parenting-app-ui/issues/1726
- */
-function isLegacyBrowser() {
-  if (typeof window === 'undefined') {
-    // server-side rendering
-    return false;
-  }
-  return (
-    typeof ReadableStream === 'undefined' || typeof Promise['allSettled'] === 'undefined' || !supportsOptionalChaining()
-  );
-}
+  private async runCompatibilityCheck() {
+    // use same check that ngx-extended-pdf calls when checking compatibility locally
 
-function supportsOptionalChaining() {
-  try {
-    eval('const foo = {}; foo?.bar');
-  } catch (e) {
-    return false;
+    // NOTE - as of v19 requires quite modern features available in native browser (pdfJS v4.1+),
+    // and cannot be polyfilled as required by worker/iframe. E.g. Promise.withResolvers
+    // https://github.com/stephanrauh/ngx-extended-pdf-viewer/issues/2500
+    // https://github.com/mozilla/pdf.js/pull/17854
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/withResolvers#browser_compatibility
+
+    // This is currently known to be resolved as of chrome 119
+
+    // Use inline scripts. Note if Using CSP security should set to false and include
+    // `op-chaining-support.js` in list of assets copied to run compatibility checks from js file instead of inline
+    const useInlineScripts = true;
+    this.legacyBrowser = await new PDFScriptLoaderService(null as any, null as any)['needsES5'](useInlineScripts);
   }
-  return true;
 }
