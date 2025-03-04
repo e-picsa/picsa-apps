9dae85d9e8ec2ad591fe911ba915922b7c2fc36b
chrismclarke <chris.m.clarke@live.co.uk>
2025-03-04 14:08:35 -0800

    fix: error handler

--- apps/picsa-apps/dashboard/src/app/modules/error-handler.ts
+++ apps/picsa-apps/dashboard/src/app/modules/error-handler.ts
@@ -2,14 +2,22 @@ import { ErrorHandler, Injectable } from '@angular/core';
 import { PicsaNotificationService } from '@picsa/shared/services/core/notification.service';
 
 @Injectable({ providedIn: 'root' })
-export class DashboardErrorHandler extends ErrorHandler {
+export class DashboardErrorHandler implements ErrorHandler {
   constructor(private notificationService: PicsaNotificationService) {
-    super();
+    // Ensure errors handled if thrown globally
+    // https://github.com/angular/angular/issues/56240
+    window.addEventListener('unhandledrejection', (e) => {
+      this.handleError(e.reason);
+      e.preventDefault();
+    });
+    window.addEventListener('error', (event) => {
+      this.handleError(event.error);
+      event.preventDefault();
+    });
   }
 
-  override handleError(error: Error) {
+  handleError(error: Error) {
     console.error(error);
     this.notificationService.showErrorNotification(error.message);
-    super.handleError(error);
   }
 }
