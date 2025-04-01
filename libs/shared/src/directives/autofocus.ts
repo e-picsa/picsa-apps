1f028402953da4f1eaf1fd4043b746396a682c9c
chrismclarke <chris.m.clarke@live.co.uk>
2025-03-31 21:52:44 -0700

    feat: add autofocus directive

--- libs/shared/src/directives/autofocus.ts
+++ libs/shared/src/directives/autofocus.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, input } from '@angular/core';
 
 /**
  * Small utility to ensure element autofocuses when page loaded
@@ -6,14 +6,26 @@ import { Directive, ElementRef } from '@angular/core';
  *
  * Adapted from:
  * https://medium.com/netanelbasal/autofocus-that-works-anytime-in-angular-apps-68cb89a3f057
+ *
+ * @example
+ * ```html
+ * <input type="text" appAutofocus />  <!-- Simple autofocus -->
+ * <input type="text" [appAutofocus]="myCondition" /> <!-- Conditional autofocus -->
+ * ```
  */
 @Directive({
-  selector: '[autofocus]',
+  selector: '[appAutoFocus]',
 })
 export class AutofocusDirective {
+  appAutofocus = input(true); //Control autofocus
   constructor(private host: ElementRef) {}
 
   ngAfterViewInit() {
     this.host.nativeElement.focus();
+    // request animation frame to ensure directive does not attempt to focus before
+    // browser has had a chance to handle initial focus handling
+    requestAnimationFrame(() => {
+      this.host.nativeElement.focus();
+    });
   }
 }
