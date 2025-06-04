e0a949b26294098419f699a1c3362cf9e0929b42
chrismclarke <chris.m.clarke@live.co.uk>
2025-06-04 07:05:40 -0700

    chore: bump dashboard version

--- apps/picsa-apps/dashboard/src/app/data/index.ts
+++ apps/picsa-apps/dashboard/src/app/data/index.ts
@@ -2,4 +2,4 @@ export * from './navLinks';
 
 // Major version should be aligned with current app major
 // Minor version used for patch updates
-export const DASHBOARD_APP_VERSION = '3.0.1';
+export const DASHBOARD_APP_VERSION = '3.0.2';
