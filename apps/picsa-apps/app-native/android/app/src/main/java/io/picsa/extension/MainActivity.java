8065f12155e2177bb22c4f25150a09ce05d471f8
chrismclarke <chris.m.clarke@live.co.uk>
2025-07-16 15:37:40 -0700

    feat: android animated splash

--- apps/picsa-apps/app-native/android/app/src/main/java/io/picsa/extension/MainActivity.java
+++ apps/picsa-apps/app-native/android/app/src/main/java/io/picsa/extension/MainActivity.java
@@ -1,5 +1,29 @@
 package io.picsa.extension;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+import androidx.annotation.Nullable;
+
+public class MainActivity extends BridgeActivity {
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    // Track how long app startup takes to adjust how long to keep splash screen displayed
+    long START_TIME = System.currentTimeMillis();
+
+    super.onCreate(savedInstanceState);
+    final View content = findViewById(android.R.id.content);
+
+    // Ensure splash is displayed for at least 800ms (default dismisses on first draw)
+    long SPLASH_MIN_DURATION = 800;
+    long END_TIME = System.currentTimeMillis();
+    long TIME_ELAPSED = END_TIME - START_TIME;
+    long SPLASH_DELAY = SPLASH_MIN_DURATION - TIME_ELAPSED;
+    if(SPLASH_DELAY > 0){
+      new SplashDelay().run(SPLASH_DELAY, content);
+    }
+  }
+}
