26ce6df7489d566ed4c91b7dced21a00467d8f1e
chrismclarke <chris.m.clarke@live.co.uk>
2025-07-17 13:02:06 -0700

    chore: code tidying

--- libs/shared/src/services/native/storage-service.ts
+++ libs/shared/src/services/native/storage-service.ts
@@ -111,6 +111,8 @@ export class NativeStorageService extends PicsaAsyncService {
         path: this.cacheName,
       });
     } catch (error: any) {
+      // Codes used from 7.1.0 onwards
+      // https://capacitorjs.com/docs/apis/filesystem#errors
       if (error?.code === 'OS-PLUG-FILE-0008') {
         await Filesystem.mkdir({
           directory: Directory.Data,
