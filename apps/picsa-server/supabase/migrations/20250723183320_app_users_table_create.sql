9ff2d876979b55506ee758988a0fcd161cc55a22
chrismclarke <chris.m.clarke@live.co.uk>
2025-08-18 21:50:18 -0700

    feat: db schema updates

--- apps/picsa-server/supabase/migrations/20250723183320_app_users_table_create.sql
+++ apps/picsa-server/supabase/migrations/20250723183320_app_users_table_create.sql
@@ -1,8 +1,17 @@
 create table
   public.app_users (
-    device_id text not null,
-    country text not null,
+    id uuid not null,
+    created_at timestamp with time zone not null default now(),
+    updated_at timestamp with time zone not null default now(),
+
+    country_code text null,
+    language_code text null,
     user_type text null,
     platform text null,
-    constraint app_users_pkey primary key (device_id)
+    
+    constraint app_users_pkey primary key (id)
   ) tablespace pg_default;
+
+  --  NOTE - required extensions - allow moddatetime
+create trigger handle_updated_at before update on public.app_users
+  for each row execute procedure moddatetime (updated_at);
