d95df76096d52e5864bb8ed956bdc6dd25e43210
chrismclarke <chris.m.clarke@live.co.uk>
2025-09-19 10:38:00 -0700

    content: picsa operational intro videos

--- libs/data/resources/videos/operational.ts
+++ libs/data/resources/videos/operational.ts
@@ -14,13 +14,55 @@ const PICSA_OPERATIONAL_VIDEOS_BASE: Record<IPicsaOperationalVideoID, Omit<IPics
         supabase_url:
           'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/global/videos/operational/Intro%20Video%20English.mp4',
       },
+      // ZM
       {
         id: '',
-        locale_codes: ['zm_ny', 'global_en'],
+        locale_codes: ['zm_ny'],
         resolution: '480p',
         size_kb: 22300,
         supabase_url:
           'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/overall.mp4',
+      },
+      {
+        id: '',
+        locale_codes: ['zm_bem'],
+        resolution: '480p',
+        size_kb: 19120,
+        supabase_url:
+          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/intro/PICSA%20Zambia%20Intro_BEM_360p.mp4',
+      },
+      {
+        id: '',
+        locale_codes: [],
+        resolution: '480p',
+        size_kb: 18100,
+        supabase_url:
+          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/intro/PICSA%20Zambia%20Intro_NYA_360p.mp4',
+      },
+      {
+        id: '',
+        locale_codes: ['zm_toi'],
+        resolution: '480p',
+        size_kb: 18950,
+        supabase_url:
+          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/intro/PICSA%20Zambia%20Intro_TON_360p.mp4',
+      },
+      // MW
+      {
+        id: '',
+        locale_codes: ['mw_ny'],
+        resolution: '480p',
+        size_kb: 25900,
+        supabase_url:
+          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/mw/videos/intro/PICSA%20Malawi%20Intro_CHE_360p.mp4',
+      },
+      {
+        id: '',
+        locale_codes: ['mw_tum'],
+        resolution: '480p',
+        size_kb: 18330,
+        supabase_url:
+          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/mw/videos/intro/PICSA%20Malawi%20Intro_TUM_360p.mp4',
       },
     ],
   },
