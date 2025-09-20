694e3b043dd38482823482ae483ba4abd3f80ae4
chrismclarke <chris.m.clarke@live.co.uk>
2025-09-20 13:07:45 -0700

    content: operational videos zm

--- libs/data/resources/videos/operational.ts
+++ libs/data/resources/videos/operational.ts
@@ -6,6 +6,7 @@ type IPicsaOperationalVideoID = 'intro' | 'historic_climate' | 'options' | 'part
 const PICSA_OPERATIONAL_VIDEOS_BASE: Record<IPicsaOperationalVideoID, Omit<IPicsaVideoData, 'id'>> = {
   intro: {
     children: [
+      // Global
       {
         id: '',
         locale_codes: ['global_en', 'global_en'],
@@ -15,19 +16,61 @@ const PICSA_OPERATIONAL_VIDEOS_BASE: Record<IPicsaOperationalVideoID, Omit<IPics
           'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/global/videos/operational/Intro%20Video%20English.mp4',
       },
       // ZM
+      // LEGACY - to remove in 2026
+      // {
+      //   id: '',
+      //   locale_codes: ['zm_ny'],
+      //   resolution: '480p',
+      //   size_kb: 22300,
+      //   supabase_url:
+      //     'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/overall.mp4',
+      // },
       {
         id: '',
         locale_codes: ['zm_ny'],
         resolution: '480p',
-        size_kb: 22300,
+        size_kb: 16470,
         supabase_url:
-          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/overall.mp4',
+          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/Intro%20Operational%20Video_Zambia_480p.mp4',
+      },
+      {
+        id: '',
+        locale_codes: ['zm_loz'],
+        resolution: '480p',
+        size_kb: 11810,
+        supabase_url:
+          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/Intro%20Operational%20Tool%20-%20LOZ_480p.mp4',
+      },
+      {
+        id: '',
+        locale_codes: ['zm_bem'],
+        resolution: '480p',
+        size_kb: 13000,
+        supabase_url:
+          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/Intro%20Operational%20Tools_BEM_480p.mp4',
+      },
+      {
+        id: '',
+        locale_codes: ['zm_toi'],
+        resolution: '480p',
+        size_kb: 16780,
+        supabase_url:
+          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/Intro%20to%20Operational%20Tools_TNG_480p.mp4',
+      },
+      {
+        id: '',
+        locale_codes: ['zm_lue'],
+        resolution: '480p',
+        size_kb: 14330,
+        supabase_url:
+          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/Introduction%20to%20Operational%20Tools%20-%20LUV_480p.mp4',
       },
     ],
   },
 
   historic_climate: {
     children: [
+      // Global
       {
         id: '',
         locale_codes: ['global_en', 'global_en'],
@@ -36,19 +79,64 @@ const PICSA_OPERATIONAL_VIDEOS_BASE: Record<IPicsaOperationalVideoID, Omit<IPics
         supabase_url:
           'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/global/videos/operational/HCI%20English.mp4',
       },
+      // ZM
+      // LEGACY - to remove in 2026
+      // {
+      //   id: '',
+      //   locale_codes: ['zm_ny', 'global_en'],
+      //   size_kb: 15730,
+      //   resolution: '480p',
+      //   supabase_url:
+      //     'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/historic-climate.mp4',
+      // },
       {
         id: '',
-        locale_codes: ['zm_ny', 'global_en'],
-        size_kb: 15730,
+        locale_codes: ['zm_ny'],
+        size_kb: 6780,
         resolution: '480p',
         supabase_url:
-          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/historic-climate.mp4',
+          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/HCI%20Zambia_480p.mp4',
       },
+      {
+        id: '',
+        locale_codes: ['zm_loz'],
+        size_kb: 7320,
+        resolution: '480p',
+        supabase_url:
+          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/HCI%20Operational%20Tool%20-%20LOZ_480p.mp4',
+      },
+      {
+        id: '',
+        locale_codes: ['zm_toi'],
+        size_kb: 9860,
+        resolution: '480p',
+        supabase_url:
+          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/HCI%20Operational%20Tool_TNG_480p.mp4',
+      },
+      {
+        id: '',
+        locale_codes: ['zm_lue'],
+        size_kb: 8190,
+        resolution: '480p',
+        supabase_url:
+          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/HCI%20Operational%20tool%20-%20LUV_480p.mp4',
+      },
+      {
+        id: '',
+        locale_codes: ['zm_bem'],
+        size_kb: 7520,
+        resolution: '480p',
+        supabase_url:
+          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/HCI%20Tool%20Operational_BEM_480p.mp4',
+      },
+
+      // MW
     ],
   },
 
   options: {
     children: [
+      // Global
       {
         id: '',
         locale_codes: ['global_en', 'global_en'],
@@ -57,18 +145,61 @@ const PICSA_OPERATIONAL_VIDEOS_BASE: Record<IPicsaOperationalVideoID, Omit<IPics
         supabase_url:
           'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/global/videos/operational/Options%20English.mp4',
       },
+      // ZM
+      // LEGACY - to remove in 2026
+      // {
+      //   id: '',
+      //   locale_codes: ['zm_ny', 'global_en'],
+      //   size_kb: 43900,
+      //   resolution: '480p',
+      //   supabase_url:
+      //     'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/option.mp4',
+      // },
       {
         id: '',
-        locale_codes: ['zm_ny', 'global_en'],
-        size_kb: 43900,
+        locale_codes: ['zm_ny'],
+        size_kb: 30390,
         resolution: '480p',
         supabase_url:
-          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/option.mp4',
+          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/Option%20operational%20video_480p.mp4',
+      },
+      {
+        id: '',
+        locale_codes: ['zm_loz'],
+        size_kb: 27940,
+        resolution: '480p',
+        supabase_url:
+          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/Options%20Operational%20Tool%20-%20LOZ_480p.mp4',
+      },
+      {
+        id: '',
+        locale_codes: ['zm_toi'],
+        size_kb: 33250,
+        resolution: '480p',
+        supabase_url:
+          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/Options%20Operational%20Tool_TNG_480p.mp4',
+      },
+      {
+        id: '',
+        locale_codes: ['zm_lue'],
+        size_kb: 33480,
+        resolution: '480p',
+        supabase_url:
+          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/Options%20Tool%20Operational%20-%20LUV_480p.mp4',
+      },
+      {
+        id: '',
+        locale_codes: ['zm_bem'],
+        size_kb: 31050,
+        resolution: '480p',
+        supabase_url:
+          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/Options%20Tool%20Operational%20-%20LUV_480p.mp4',
       },
     ],
   },
   participatory_budget: {
     children: [
+      // Global
       {
         id: '',
         locale_codes: ['global_en', 'global_en'],
@@ -77,12 +208,54 @@ const PICSA_OPERATIONAL_VIDEOS_BASE: Record<IPicsaOperationalVideoID, Omit<IPics
         supabase_url:
           'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/global/videos/operational/PB%20Operational%20Video.mp4',
       },
+      // ZM
+      // LEGACY - to remove in 2026
+      // {
+      //   id: '',
+      //   locale_codes: ['zm_ny', 'global_en'],
+      //   size_kb: 43410,
+      //   resolution: '480p',
+      //   supabase_url: 'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/pb.mp4',
+      // },
       {
         id: '',
-        locale_codes: ['zm_ny', 'global_en'],
-        size_kb: 43410,
+        locale_codes: ['zm_loz'],
+        size_kb: 21890,
         resolution: '480p',
-        supabase_url: 'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/pb.mp4',
+        supabase_url:
+          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/PB%20Operational%20Tool%20-%20LOZ_480p.mp4',
+      },
+      {
+        id: '',
+        locale_codes: ['zm_toi'],
+        size_kb: 29240,
+        resolution: '480p',
+        supabase_url:
+          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/PB%20Operational%20Tool_TNG_480p.mp4',
+      },
+      {
+        id: '',
+        locale_codes: ['zm_ny'],
+        size_kb: 20800,
+        resolution: '480p',
+        supabase_url:
+          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/PB%20Operational%20video_480p.mp4',
+      },
+      {
+        id: '',
+        locale_codes: ['zm_bem'],
+        size_kb: 24500,
+        resolution: '480p',
+        supabase_url:
+          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/PB%20Tool%20Operational_BEM_480p.mp4',
+      },
+      {
+        id: '',
+        locale_codes: ['zm_lue'],
+        size_kb: 26260,
+        resolution: '480p',
+        supabase_url:
+          'https://wpctacqpzxfzlucblowh.supabase.co/storage/v1/object/public/zm/videos/operational/Participatory%20Budget%20Tool%20%20-%20LUV_480p.mp4',
       },
     ],
   },
