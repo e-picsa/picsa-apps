e8da50207be1ccc68db883fce2554addeb08f2ce
chrismclarke <chris.m.clarke@live.co.uk>
2025-11-22 12:50:07 -0800

    chore: code tidying

--- apps/picsa-tools/forecasts-tool/src/app/schemas/schema_v2.ts
+++ apps/picsa-tools/forecasts-tool/src/app/schemas/schema_v2.ts
@@ -6,18 +6,21 @@ import { COLLECTION_V1, IForecast_V1, SCHEMA_V1 } from './schema_v1';
 
 const SCHEMA_VERSION = 2;
 
-// add downscaled_location column
-export interface IForecast_V2 extends IForecast_V1 {
+// remove 'location' column and populate 'downscaled_location' column
+export interface IForecast_V2 extends Omit<IForecast_V1, 'location'> {
   downscaled_location: string | null;
 }
 
+const { location, ...keptProperties } = SCHEMA_V1.properties;
 export const SCHEMA_V2: RxJsonSchema<IForecast_V2> = {
   ...SCHEMA_V1,
   version: SCHEMA_VERSION,
   properties: {
-    ...SCHEMA_V1.properties,
+    ...keptProperties,
     downscaled_location: { type: 'string' },
   },
+  required: ['id', 'country_code', 'storage_file'],
+  primaryKey: 'id',
 };
 
 export const COLLECTION_V2: IPicsaCollectionCreator<IForecast_V2> = {
@@ -25,15 +28,15 @@ export const COLLECTION_V2: IPicsaCollectionCreator<IForecast_V2> = {
   isUserCollection: false,
   migrationStrategies: {
     ...COLLECTION_V1.migrationStrategies,
-    2: (legacyDoc: IForecast_V1): IForecast_V2 => ({
-      ...legacyDoc,
+    2: ({ location, ...keptProperties }: IForecast_V1): IForecast_V2 => ({
+      ...keptProperties,
       downscaled_location: null,
     }),
   },
 };
 
-export const SERVER_DB_MAPPING_V2 = (row: IForecastRow): IForecast_V2 => ({
-  ...row,
+export const SERVER_DB_MAPPING_V2 = ({ location, ...keptProperties }: IForecastRow): IForecast_V2 => ({
+  ...keptProperties,
   // null storage files filtered during db query
-  storage_file: row.storage_file as string,
+  storage_file: keptProperties.storage_file as string,
 });
