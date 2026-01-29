40fc5f5688b01796493a45079508755aab5b3e36
chrismclarke <chris.m.clarke@live.co.uk>
2026-01-28 17:02:16 -0800

    chore: tidying

--- apps/picsa-apps/dashboard/src/app/modules/landing/landing.component.ts
+++ apps/picsa-apps/dashboard/src/app/modules/landing/landing.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { APP_VERSION } from '@picsa/environments/src/version';
@@ -9,7 +10,7 @@ import { DashboardSignInComponent } from '../auth/components/sign-in/sign-in.com
 @Component({
   selector: 'dashboard-landing-page',
   standalone: true,
-  imports: [ReactiveFormsModule, DashboardSignInComponent, MatCardModule, MatToolbarModule],
+  imports: [ReactiveFormsModule, DashboardSignInComponent, MatCardModule, MatToolbarModule, MatButtonModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
