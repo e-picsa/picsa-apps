33d74c603e32003e78c65240415d83d64af1eee1
chrismclarke <chris.m.clarke@live.co.uk>
2026-03-05 14:25:03 -0800

    feat: derived db types

--- apps/picsa-server/supabase/types/index.ts
+++ apps/picsa-server/supabase/types/index.ts
@@ -1,12 +1,14 @@
-import type {Database} from './db.types.ts'
-export type {Database}
+import type { Database } from './db.types.ts';
+export type { Database };
+
+import type { CountryCode, CountryCodeLegacy, LocaleCode, LocaleCodeLegacy } from './db-derived.types.ts';
+export type { CountryCode, CountryCodeLegacy, LocaleCode, LocaleCodeLegacy };
 
 // Utility type
-export type AppRole = Database['public']['Enums']['app_role']
+export type AppRole = Database['public']['Enums']['app_role'];
 
-import type {FunctionResponses} from './functions.types.ts'
-export type {FunctionResponses}
+import type { FunctionResponses } from './functions.types.ts';
+export type { FunctionResponses };
 
-import type * as ClimateApi from "./climate-api.types.ts";
+import type * as ClimateApi from './climate-api.types.ts';
 export type { ClimateApi };
-
