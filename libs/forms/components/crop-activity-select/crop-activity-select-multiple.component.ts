44b306db4df627cb95384237fa66181a58eb3f4e
chrismclarke <chris.m.clarke@live.co.uk>
2026-03-30 21:48:17 -0700

    feat: form component multiple variants

--- libs/forms/components/crop-activity-select/crop-activity-select-multiple.component.ts
+++ libs/forms/components/crop-activity-select/crop-activity-select-multiple.component.ts
@@ -2,24 +2,24 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
-import { IWeatherDataEntry, WEATHER_DATA } from '@picsa/data/weather';
+import { CROP_ACTIVITY_DATA, CROP_ACTIVITY_HASHMAP, ICropActivityDataEntry } from '@picsa/data';
 import { PicsaTranslateModule } from '@picsa/i18n';
 
-import { PicsaFormBaseSelectComponent } from '../base/select';
+import { PicsaFormBaseSelectMultipleComponent } from '../base/select-multiple';
 
 @Component({
-  selector: 'picsa-form-weather-select',
-  templateUrl: './weather-select.component.html',
-  styleUrls: ['./weather-select.scss'],
+  selector: 'picsa-form-crop-activity-select-multiple',
+  templateUrl: './crop-activity-select-multiple.component.html',
+  styleUrls: ['./crop-activity-select.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   imports: [MatButtonModule, MatIconModule, MatDialogModule, PicsaTranslateModule],
 })
-export class FormWeatherSelectComponent extends PicsaFormBaseSelectComponent<IWeatherDataEntry> {
+export class FormCropActivitySelectMultipleComponent extends PicsaFormBaseSelectMultipleComponent<ICropActivityDataEntry> {
   dialog = inject(MatDialog);
 
   constructor() {
     super();
-    this.initBase(WEATHER_DATA.filter((v) => !!v.label));
+    this.initBase(CROP_ACTIVITY_DATA, CROP_ACTIVITY_HASHMAP);
   }
 }
--- libs/forms/components/weather-select/weather-select-multiple.component.ts
+++ libs/forms/components/weather-select/weather-select-multiple.component.ts
@@ -2,24 +2,31 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
-import { IWeatherDataEntry, WEATHER_DATA } from '@picsa/data/weather';
+import { IWeatherDataEntry, WEATHER_DATA, WEATHER_DATA_HASHMAP } from '@picsa/data/weather';
 import { PicsaTranslateModule } from '@picsa/i18n';
 
-import { PicsaFormBaseSelectComponent } from '../base/select';
+import { PicsaFormBaseSelectMultipleComponent } from '../base/select-multiple';
 
 @Component({
-  selector: 'picsa-form-weather-select',
-  templateUrl: './weather-select.component.html',
+  selector: 'picsa-form-weather-select-multiple',
+  templateUrl: './weather-select-multiple.component.html',
   styleUrls: ['./weather-select.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   imports: [MatButtonModule, MatIconModule, MatDialogModule, PicsaTranslateModule],
 })
-export class FormWeatherSelectComponent extends PicsaFormBaseSelectComponent<IWeatherDataEntry> {
+export class FormWeatherSelectMultipleComponent extends PicsaFormBaseSelectMultipleComponent<IWeatherDataEntry> {
   dialog = inject(MatDialog);
 
   constructor() {
     super();
-    this.initBase(WEATHER_DATA.filter((v) => !!v.label));
+    this.initBase(
+      WEATHER_DATA.filter((v) => !!v.label),
+      WEATHER_DATA_HASHMAP,
+    );
+  }
+
+  public handleSelect(id: string) {
+    this.toggleSelected(id);
   }
 }
