de5bec820ab3c23b8decaa535a7fccb90dc90121
Chris Clarke <chris.m.clarke@live.co.uk>
2026-06-07 21:20:15 -0700

    chore: update zw climate definitions (#631)

--- libs/data/climate/chart_definitions/zw.ts
+++ libs/data/climate/chart_definitions/zw.ts
@@ -4,4 +4,10 @@ const definitions = DEFAULT_DEFINITIONS();
 
 // Provide country-specific overrides for definitions and labels
 
+definitions.start.definition =
+  'First occasion, after start of rains and before 31 Jan with more than 20mm rain in 3 days and no dry spell longer than 10 days in the next 21 days';
+
+definitions.end.definition =
+  'First occasion, after end of rains and before 30 June when soil moisture level is less than zero';
+
 export default definitions;
