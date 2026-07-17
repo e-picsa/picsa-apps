5f921cff4321f9fe3ec050ef482a2b15bd6b2f81
Chris Clarke <chris.m.clarke@live.co.uk>
2026-07-17 17:19:00 +0100

    chore: update climate translations (#647)

--- libs/data/climate/chart_definitions/zm.ts
+++ libs/data/climate/chart_definitions/zm.ts
@@ -3,12 +3,12 @@ import DEFAULT_DEFINITIONS from './default';
 const definitions = DEFAULT_DEFINITIONS();
 
 definitions.end.definition =
-  'First occurence from 1 Mar where water balance reduces to 0.5 mm from an initial capacity of 100mm with an assumed evaporation rate of 5mm/day';
+  'First occurrence from 1 Mar where water balance reduces to 0.5 mm from an initial capacity of 100mm with an assumed evaporation rate of 5mm/day';
 definitions.extreme_rainfall_days.definition = '';
 definitions.length.definition = 'Number of days between start of the rains and end of the season';
 definitions.rainfall.definition = 'Total rainfall per year from start of the rains to end of the season';
 definitions.start.definition =
-  'First occurence from 1 Oct with at least 20 mm of rainfall over 3 consecutive days (with no dry spell of length 9 in the next 21 days)';
+  'First occurrence from 1 Oct with at least 20 mm of rainfall over 3 consecutive days (with no dry spell of length 9 in the next 21 days)';
 definitions.temp_max.definition = 'The mean maximum and highest maximum daily temperatures per year';
 definitions.temp_min.definition = 'The mean minimum and lowest minimum daily temperatures per year';
 
