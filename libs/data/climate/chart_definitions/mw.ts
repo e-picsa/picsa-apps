91641e0ed4afaafebf7eded2e1c00f8791c18c17
chrismclarke <chris.m.clarke@live.co.uk>
2026-07-17 16:41:55 +0100

    fix: typos

--- libs/data/climate/chart_definitions/mw.ts
+++ libs/data/climate/chart_definitions/mw.ts
@@ -4,12 +4,12 @@ const definitions = DEFAULT_DEFINITIONS();
 
 definitions.extreme_rainfall_days.keys = ['sum_RD50' as any];
 
-definitions.end.definition = 'Last occurance between 1 Oct and 30 Apr with 10 mm rainfall';
+definitions.end.definition = 'Last occurence between 1 Oct and 30 Apr with 10 mm rainfall';
 definitions.extreme_rainfall_days.definition = 'Rainy days with at least 50mm';
 definitions.length.definition = 'Number of days between start and end of the rains';
 definitions.rainfall.definition = 'Total rainfall per year from start of the rains to end of the season';
 definitions.start.definition =
-  'First occurance from 1 Oct with at least 25 mm of rainfall over 3 consecutive days (with no dry spell of length 9 in the next 21 days)';
+  'First occurence from 1 Oct with at least 25 mm of rainfall over 3 consecutive days (with no dry spell of length 9 in the next 21 days)';
 definitions.temp_max.definition = 'The mean maximum and highest maximum daily temperatures per year';
 definitions.temp_min.definition = 'The mean minimum and lowest minimum daily temperatures per year';
 
