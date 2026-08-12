2c63215d5a23f0cbfe35f7cf203e874c20f21706
Chris Clarke <chris.m.clarke@live.co.uk>
2026-08-12 18:45:59 +0100

    fix: climate specs (#675)

--- apps/picsa-tools/climate-tool/src/app/components/chart-tools/terciles-tool/terciles-tool.component.spec.ts
+++ apps/picsa-tools/climate-tool/src/app/components/chart-tools/terciles-tool/terciles-tool.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SocialSharing } from '@awesome-cordova-plugins/social-sharing/ngx';
+import { PicsaTranslateModule } from '@picsa/i18n';
 
 import { TercilesToolComponent } from './terciles-tool.component';
 
@@ -8,7 +10,8 @@ describe('TercilesToolComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [TercilesToolComponent],
+      imports: [TercilesToolComponent, PicsaTranslateModule.forRoot()],
+      providers: [{ provide: SocialSharing, useValue: {} }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TercilesToolComponent);
--- apps/picsa-tools/climate-tool/src/app/components/chart-tools/tool-select/tool-select.component.spec.ts
+++ apps/picsa-tools/climate-tool/src/app/components/chart-tools/tool-select/tool-select.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PicsaTranslateModule } from '@picsa/i18n';
 
 import { ToolSelectComponent } from './tool-select.component';
 
@@ -8,7 +9,7 @@ describe('ToolSelectComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ToolSelectComponent],
+      imports: [ToolSelectComponent, PicsaTranslateModule.forRoot()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ToolSelectComponent);
